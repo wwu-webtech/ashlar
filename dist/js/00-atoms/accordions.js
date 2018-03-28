1a50592fb359622006a855781b74d31a2417300c
masons7 <itsstef@gmail.com>
2018-03-28 16:08:17 -0700

    moved accordion items to the molecule level

--- dist/js/00-atoms/accordions.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-  $(".expand").click(function(){
-    $(".expand + .content").slideToggle();
-    $(".expand").toggleClass("is-expanded");
-
-    $(".expand .material-icons").text("add");
-    $(".expand.is-expanded .material-icons").text("clear");
-  });
-});
