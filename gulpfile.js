c2e6fdfa57e6ea5549c04591d70abb4b5a04de66
Nigel Packer <nigel.packer@wwu.edu>
2018-05-18 09:29:56 -0700

    Improve gulpfile.
    Remove dependency on deprecated gulp-clean.
    Use pump for better error messages.
    Remove unnecessary "./" from all file paths.

--- gulpfile.js
+++ gulpfile.js
@@ -3,9 +3,11 @@
   'use strict';
 
   const path = require('path');
+  const pump = require('pump');
   const sequence = require('run-sequence');
   const browserify = require('browserify');
   const source = require('vinyl-source-stream');
+  const del = require('del');
   const gulp = require('gulp');
   const run = require('gulp-run');
   const tap = require('gulp-tap');
@@ -17,47 +19,47 @@
   const sourcemaps = require('gulp-sourcemaps');
   const sass = require('gulp-sass');
   const sassGlob = require('gulp-sass-glob');
-  const clean = require('gulp-clean');
+
+  function drupalBehaviorName(file) {
+    return path.basename(file.path, '.js').split('.').pop();
+  }
 
   var config = {};
 
-  config.patterns = {
-    dir: './source/_patterns'
-  };
-
   config.patternLab = {
+    patterns: 'source/_patterns',
     sassFile: 'wwu-styleguide.css',
     sassSrc: [
-      './source/sass/**/*.scss'
+      'source/sass/**/*.scss'
     ],
-    sassDest: './source/pattern-lab/css',
+    sassDest: 'source/pattern-lab/css',
     jsFile: 'wwu-styleguide.js',
     jsSrc: [
-      './source/js/*.js'
+      'source/js/*.js'
     ],
-    jsDest: './source/pattern-lab/js',
+    jsDest: 'source/pattern-lab/js',
     jsTemplate: {
-      src: './source/js/patternlab.lodash'
+      src: 'source/js/patternlab.lodash'
     }
   };
 
   config.sass = {
     src: [
-      './source/sass/*.scss',
-      '!./source/sass/styleguide/*'
+      'source/sass/*.scss',
+      '!source/sass/styleguide/*'
     ],
-    dest: './build/css',
+    dest: 'build/css',
     watch: [
-      './source/sass/**/*.scss',
-      config.patterns.dir + '/**/*.scss',
+      'source/sass/**/*.scss',
+      config.patternLab.patterns + '/**/*.scss',
     ],
     options: {
       includePaths: [
-        './source/sass',
-        './node_modules/shila-css',
-        './node_modules/breakpoint-sass/stylesheets',
-        './node_modules/sass-toolkit/stylesheets',
-        './node_modules/singularitygs/stylesheets'
+        'source/sass',
+        'node_modules/shila-css',
+        'node_modules/breakpoint-sass/stylesheets',
+        'node_modules/sass-toolkit/stylesheets',
+        'node_modules/singularitygs/stylesheets'
       ],
       outputStyle: 'expanded'
     }
@@ -65,14 +67,14 @@
 
   config.js = {
     src: [
-      './source/js/*.js'
+      'source/js/*.js'
     ],
-    dest: './build/js',
+    dest: 'build/js',
     watch: [
-      './source/js/**/*.js'
+      'source/js/**/*.js'
     ],
     template: {
-      src: './source/js/behavior.lodash'
+      src: 'source/js/behavior.lodash'
     },
     templateVariable: 'behavior',
     rename: {
@@ -83,85 +85,99 @@
       trimCode: true,
       params: ['$', 'Drupal', 'window', 'document', 'undefined'],
       args: ['jQuery', 'Drupal', 'this', 'this.document']
-    }
-  };
-
-  config.uglify = {
-    compress: false,
-    mangle: false,
-    output: {
-      beautify: true,
-      braces: true,
-      indent_level: 2,
+    },
+    uglify: {
+      compress: false,
+      mangle: false,
+      output: {
+        beautify: true,
+        braces: true,
+        comments: true,
+        indent_level: 2
+      }
     }
   };
 
   /**
    * Generate CSS.
    */
-  gulp.task('sass', function () {
-    return gulp.src(config.sass.src)
-    .pipe(sassGlob())
-    .pipe(sourcemaps.init())
-    .pipe(sass(config.sass.options).on('error', sass.logError))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.sass.dest));
+  gulp.task('sass', function (callback) {
+    pump(
+      gulp.src(config.sass.src),
+      sassGlob(),
+      sourcemaps.init(),
+      sass(config.sass.options),
+      sourcemaps.write(),
+      gulp.dest(config.sass.dest),
+      callback
+    );
   });
 
   /**
    * Generate JS.
    */
-  gulp.task('js', function () {
-    return gulp.src(config.js.src)
-    .pipe(tap(function (file) {
-      var behaviorName = path.basename(file.path, '.js').split('.').pop();
-
-      return gulp.src(file.path)
-      .pipe(wrap(config.js.template, { name: behaviorName }, { variable: config.js.templateVariable }))
-      .pipe(iife(config.js.iife))
-      .pipe(uglify(config.uglify))
-      .pipe(rename(config.js.rename))
-      .pipe(gulp.dest(config.js.dest));
-    }));
+  gulp.task('js', function (callback) {
+    pump(
+      gulp.src(config.js.src),
+      tap(function (file) {
+        return pump(
+          gulp.src(file.path),
+          wrap(config.js.template, { name: drupalBehaviorName(file) }, { variable: config.js.templateVariable }),
+          iife(config.js.iife),
+          uglify(config.js.uglify),
+          gulp.dest(config.js.dest)
+        );
+      }),
+      callback
+    );
   });
 
   /**
    * Generate Pattern Lab CSS.
    */
-  gulp.task('patternlab:sass', function () {
-    return gulp.src(config.patternLab.sassSrc)
-    .pipe(sassGlob())
-    .pipe(sourcemaps.init())
-    .pipe(sass(config.sass.options).on('error', sass.logError))
-    .pipe(concat(config.patternLab.sassFile))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.patternLab.sassDest));
+  gulp.task('patternlab:sass', function (callback) {
+    pump(
+      gulp.src(config.patternLab.sassSrc),
+      sassGlob(),
+      sourcemaps.init(),
+      sass(config.sass.options),
+      concat(config.patternLab.sassFile),
+      sourcemaps.write(),
+      gulp.dest(config.patternLab.sassDest),
+      callback
+    );
   });
 
   /**
-   * Generate Pattern Lab JS: concatenate, wrap, and format.
+   * Generate Pattern Lab JS pre.
    */
-  gulp.task('patternlab:js:pre', function () {
-    return gulp.src(config.patternLab.jsSrc)
-    .pipe(sourcemaps.init())
-    .pipe(concat(config.patternLab.jsFile))
-    .pipe(wrap(config.patternLab.jsTemplate))
-    .pipe(uglify(config.uglify))
-    .pipe(sourcemaps.write())
-    .pipe(gulp.dest(config.patternLab.jsDest));
+  gulp.task('patternlab:js:pre', function (callback) {
+    pump(
+      gulp.src(config.patternLab.jsSrc),
+      sourcemaps.init(),
+      concat(config.patternLab.jsFile),
+      wrap(config.patternLab.jsTemplate),
+      uglify(config.js.uglify),
+      sourcemaps.write(),
+      gulp.dest(config.patternLab.jsDest),
+      callback
+    );
   });
 
   /**
-   * Generate Pattern Lab JS: browserify (include "required" libraries).
+   * Generate Pattern Lab JS post.
    */
-  gulp.task('patternlab:js:post', ['patternlab:js:pre'], function () {
-    return browserify(config.patternLab.jsDest + '/' + config.patternLab.jsFile, { debug: true }).bundle()
-    .pipe(source(config.patternLab.jsFile))
-    .pipe(gulp.dest(config.patternLab.jsDest));
+  gulp.task('patternlab:js:post', ['patternlab:js:pre'], function (callback) {
+    pump(
+      browserify(config.patternLab.jsDest + '/' + config.patternLab.jsFile, { debug: true }).bundle(),
+      source(config.patternLab.jsFile),
+      gulp.dest(config.patternLab.jsDest),
+      callback
+    );
   });
 
   /**
-   * Generate Pattern Lab JS: wrapper task.
+   * Generate Pattern Lab JS.
    */
   gulp.task('patternlab:js', ['patternlab:js:post']);
 
@@ -182,22 +198,27 @@
    * Clean generated CSS files.
    */
   gulp.task('clean:css', function () {
-    return gulp.src([config.sass.dest], { read: false })
-    .pipe(clean());
+    return del([config.sass.dest]);
   });
 
   /**
    * Clean generated JS files.
    */
   gulp.task('clean:js', function () {
-    return gulp.src([config.js.dest], { read: false })
-    .pipe(clean());
+    return del([config.js.dest]);
+  });
+
+  /**
+   * Clean generated Pattern Lab files.
+   */
+  gulp.task('clean:patternlab', function () {
+    return del([config.patternLab.sassDest, config.patternLab.jsDest]);
   });
 
   /**
    * Run clean tasks.
    */
-  gulp.task('clean', ['clean:css', 'clean:js']);
+  gulp.task('clean', ['clean:css', 'clean:js', 'clean:patternlab']);
 
   /**
    * Gulp default task.
