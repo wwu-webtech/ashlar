9e6cc1b923d79602b70fb98577899bef9ea94993
masons7 <itsstef@gmail.com>
2018-07-23 13:42:15 -0700

    search box accessibility improvements

--- source/js/01-molecules.wwu-search.js
+++ source/js/01-molecules.wwu-search.js
@@ -6,15 +6,18 @@ $(".toggle-search").click(function() {
   }
 
   $(this).toggleClass('is-expanded');
+
   $(this).siblings('.search-area').animate({width: 'toggle'});
   $(this).children('.material-icons').text('search');
   $(this).filter('.is-expanded').children('.material-icons').text('close');
 
   if (expanded) {
     $(this).parent().removeClass('is-expanded');
+    $(this).attr('aria-label', 'Open the search box');
     expanded = false;
   }
   else {
+    $(this).attr('aria-label', 'Close the search box');
     expanded = true;
   }
 });
