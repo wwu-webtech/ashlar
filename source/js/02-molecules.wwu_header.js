f7baa4df96c0543cb234fb6ff843fb77ac471c69
masons7 <itsstef@gmail.com>
2018-08-21 10:16:24 -0700

    better toggle animation

--- source/js/02-molecules.wwu_header.js
+++ source/js/02-molecules.wwu_header.js
@@ -1,19 +1,21 @@
-var $toggle_menu = $('.toggle-menu', context);
+var $toggle_open_menu = $('.toggle-open-menu', context);
+var $toggle_close_menu = $('.toggle-close-menu', context);
 
-function toggle_menu(event) {
-  $toggle_menu.off('click', toggle_menu);
+function toggle_open_menu(event) {
+  $toggle_open_menu.off('click', toggle_open_menu);
 
   $('.nav--main').animate(
     {
-      width: 'toggle',
+      left: '0',
+      opacity: '1'
     },
     {
       duration: 'normal',
       easing: 'swing',
       complete: function () {
-        $toggle_menu.toggleClass('.is-expanded');
+        $toggle_open_menu.addClass('.is-expanded');
 
-        $toggle_menu.on('click', toggle_menu);
+        $toggle_open_menu.on('click', toggle_open_menu);
       }
     }
   );
@@ -21,4 +23,27 @@ function toggle_menu(event) {
   event.stopPropagation();
 }
 
-$toggle_menu.on('click', toggle_menu);
+function toggle_close_menu(event) {
+  $toggle_close_menu.off('click', toggle_close_menu);
+
+  $('.nav--main').animate(
+    {
+      left: '-100%',
+      opacity: '0'
+    },
+    {
+      duration: 'normal',
+      easing: 'swing',
+      complete: function () {
+        $toggle_close_menu.removeClass('.is-expanded');
+
+        $toggle_close_menu.on('click', toggle_close_menu);
+      }
+    }
+  );
+
+  event.stopPropagation();
+}
+
+$toggle_open_menu.on('click', toggle_open_menu);
+$toggle_close_menu.on('click', toggle_close_menu);
