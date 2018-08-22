c58522da1f7a52eddc958cbad946cf91fff8ead7
masons7 <itsstef@gmail.com>
2018-08-22 15:58:34 -0700

    ported through to drupal, with some bugs

--- source/js/02-organisms.menu_extras.js
+++ source/js/02-organisms.menu_extras.js
@@ -1,29 +1,4 @@
-$(".nav--main").accessibleMegaMenu({
-    /* prefix for generated unique id attributes, which are required
-       to indicate aria-owns, aria-controls and aria-labelledby */
-    uuidPrefix: "accessible-megamenu",
-
-    /* css class used to define the megamenu styling */
-    menuClass: "ultimenu",
-
-    /* css class for a top-level navigation item in the megamenu */
-    topNavItemClass: "ultimenu__item",
-
-    /* css class for a megamenu panel */
-    panelClass: "ultimenu__flyout",
-
-    /* css class for a group of items within a megamenu panel */
-    panelGroupClass: "sub-nav-group",
-
-    /* css class for the hover state */
-    hoverClass: "hover",
-
-    /* css class for the focus state */
-    focusClass: "focus",
-
-    /* css class for the open state */
-    openClass: "open"
-});
+/* Mobile menu scripts */
 var $ultimenu_link = $('.ultimenu__link', context);
 var $close_flyout = $('.close-flyout', context);
 
@@ -63,3 +38,32 @@ function close_mobile_flyout(event) {
 
 $ultimenu_link.on('click', open_mobile_flyout);
 $close_flyout.on('click', close_mobile_flyout);
+
+/* Ultimenu variable declarations */
+
+$(".nav--main").accessibleMegaMenu({
+    /* prefix for generated unique id attributes, which are required
+       to indicate aria-owns, aria-controls and aria-labelledby */
+    uuidPrefix: "accessible-megamenu",
+
+    /* css class used to define the megamenu styling */
+    menuClass: "ultimenu",
+
+    /* css class for a top-level navigation item in the megamenu */
+    topNavItemClass: "ultimenu__item",
+
+    /* css class for a megamenu panel */
+    panelClass: "ultimenu__flyout",
+
+    /* css class for a group of items within a megamenu panel */
+    panelGroupClass: "sub-nav-group",
+
+    /* css class for the hover state */
+    hoverClass: "hover",
+
+    /* css class for the focus state */
+    focusClass: "focus",
+
+    /* css class for the open state */
+    openClass: "open"
+});
