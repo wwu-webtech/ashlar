e77c09c77ad36ece8bc76247ad5f17c8ddeacbd0
masons7 <masons7@wwu.edu>
2018-12-05 15:32:40 -0800

    javascript for advanced search toggling

--- source/_patterns/01-molecules/search/mabel-search/mabel-search.js
+++ source/_patterns/01-molecules/search/mabel-search/mabel-search.js
@@ -1,5 +1,12 @@
-var $toggle_advanced_search = $('#block-islandora-solr-advanced > h2', context);
+var $advanced_h2 = $('#block-islandora-solr-advanced > h2', context);
 var $advanced_search_form = $('#islandora-solr-advanced-search-form', context);
+
+$advanced_search_form.removeClass('is-expanded');
+/* Turn h2 into a button */
+$advanced_h2.replaceWith('<button aria-label="open advanced search" class="toggle-advanced-search">Advanced search<i class="material-icons" aria-hidden="true">add</i></button>');
+
+var $toggle_advanced_search = $('.toggle-advanced-search');
+var $advanced_search_icon = $toggle_advanced_search.children('.material-icons');
 
 function toggle_advanced_mabel_search(event) {
   $toggle_advanced_search.off('click', toggle_advanced_mabel_search);
@@ -12,13 +19,15 @@ function toggle_advanced_mabel_search(event) {
       duration: 'normal',
       easing: 'swing',
       complete: function () {
-        $advanced_search_form.toggleClass('.is-expanded');
+        $advanced_search_form.toggleClass('is-expanded');
 
-        if ($advanced_search_form.hasClass('.is-expanded')) {
-          $advanced_search_form.attr('aria-label', 'Close advanced search');
+        if ($advanced_search_form.hasClass('is-expanded')) {
+          $toggle_advanced_search.attr('aria-label', 'Close advanced search');
+          $advanced_search_icon.text('close');
         }
         else {
-          $advanced_search_form.attr('aria-label', 'Open advanced search');
+          $toggle_advanced_search.attr('aria-label', 'Open advanced search');
+          $advanced_search_icon.text('add');
         }
 
         $toggle_advanced_search.on('click', toggle_advanced_mabel_search);
