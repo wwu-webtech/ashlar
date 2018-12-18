34e5eb86a850e7397e08320889e9f9d585a7f4ee
Carly Gerard <carly.gerard@wwu.edu>
2018-12-17 16:29:59 -0800

    set proper focus on open/close, hiding content behind menu

--- source/_patterns/02-organisms/headers/wwu-header/wwu-header.js
+++ source/_patterns/02-organisms/headers/wwu-header/wwu-header.js
@@ -1,7 +1,31 @@
 var $toggle_open_menu = $('.toggle-open-menu', context);
 var $toggle_close_menu = $('.toggle-close-menu', context);
+var $last_focused;
+var $mobile_nav = $('.quick-links .mobile');
+var $inputs = $(document).find('select, button, a, input, textarea')
+var $page = $('.western-header, .splash, .toggle-open-menu, .search, .page-content, .region--footer');
 
+
+// function restrict_focus_mobile(event) {
+  // var $mywestern = $('.mywestern');
+  // var $last_nav_item = $('.ultimenu li').last();
+  //
+  //   $last_nav_item.on('keydown', function (event) {
+  //     if (e.which === 9 && !e.shiftKey) {
+  //       e.preventDefault();
+  //       $mywestern.focus();
+  //     }
+  //   });
+  //
+  //   $mywestern.on('keydown', function (event) {
+  //     if (e.which === 9 && e.shiftKey) {
+  //       e.preventDefault();
+  //       $last_nav_item.focus();
+  //     }
+  //   });
+  // }
 function toggle_open_menu(event) {
+  $last_focused = document.activeElement;
   $toggle_open_menu.off('click', toggle_open_menu);
   $('.nav--main').show();
   $('.nav--main').animate(
@@ -14,11 +38,14 @@ function toggle_open_menu(event) {
       easing: 'swing',
       complete: function () {
         $toggle_open_menu.addClass('is-expanded');
-
         $toggle_open_menu.on('click', toggle_open_menu);
+        $toggle_close_menu.focus();
+        $inputs.not('.nav--main :focusable').attr('tabindex', '-1');
+        $page.not('.nav--main').attr('aria-hidden', 'true');
       }
     }
   );
+    // restrict_focus_mobile();
 }
 
 function toggle_close_menu(event) {
@@ -35,8 +62,10 @@ function toggle_close_menu(event) {
       complete: function () {
         $('.nav--main').hide();
         $toggle_close_menu.removeClass('is-expanded');
-
+        $page.not('.nav--main').removeAttr('aria-hidden');
+        $inputs.not('.nav--main :focusable').removeAttr('tabindex');
         $toggle_close_menu.on('click', toggle_close_menu);
+        $last_focused.focus();
       }
     }
   );
