720865ba50a74a887f7844cd007aec69201555e1
Nigel Packer <packern@wwu.edu>
2019-01-22 16:29:51 -0800

    Add timeout to event tracking script

--- source/js/event-tracking.js
+++ source/js/event-tracking.js
@@ -7,16 +7,23 @@ var $links = $('a[data-tracking-event-category]', context);
 $links.once('tracking-event-category').on('click', function (event) {
   var $target = $(event.target).closest('a');
   var category = $target.data('tracking-event-category');
+  var locationSet = false;
+
+  function setLocation() {
+    if (!locationSet) {
+      locationSet = true;
+      window.location = $target.attr('href');
+    }
+  }
+
+  event.preventDefault();
+  setTimeout(setLocation, 2000);
 
   ga('send', {
     hitType: 'event',
     eventCategory: category,
     eventAction: 'Click',
     eventLabel: $target.text(),
-    hitCallback: function () {
-      window.location = $target.attr('href');
-    }
+    hitCallback: setLocation
   });
-
-  return false;
 });
