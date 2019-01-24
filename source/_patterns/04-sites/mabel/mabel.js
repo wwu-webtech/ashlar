9af1ec1e1558ec87fd5e43d67d9dbb4a3ae0749f
masons7 <masons7@wwu.edu>
2019-01-24 10:40:00 -0800

    styles for search navigation

--- source/_patterns/04-sites/mabel/mabel.js
+++ source/_patterns/04-sites/mabel/mabel.js
@@ -1,19 +1,34 @@
 /* Move compound object navigation out of tabs and next to JAIL display */
 var $jail = $('#block-islandora-compound-object-compound-jail-display');
-var $prev = $('#islandora-compound-previous-link');
-var $seq = $('#islandora-compound-sequence-position');
-var $next = $('#islandora-compound-next-link');
+var $jail_prev = $('#islandora-compound-previous-link');
+var $jail_seq = $('#islandora-compound-sequence-position');
+var $jail_next = $('#islandora-compound-next-link');
 var $nav_container = $('<div class="compound-navigation"></div>');
 
 $nav_container.addClass('compound-navigation');
 
 $nav_container.insertBefore($jail);
 
-$nav_container.append($prev);
-$nav_container.append($seq);
-$nav_container.append($next);
+$nav_container.append($jail_prev);
+$nav_container.append($jail_seq);
+$nav_container.append($jail_next);
+
+/* Give the manage parent link button styles */
+var $manage_parent = $('#block-islandora-compound-object-compound-jail-display a:contains("manage parent")');
+
+$manage_parent.addClass("button");
 
 /* Remove clip icon and make it a descriptive link */
 var $clip = $('#clip');
 
 $clip.html("Download and Print Options");
+
+
+/* Add button styles to search navigation links */
+var $search_return = $('#islandora-solr-search-return-link a');
+var $search_next = $('#islandora-solr-search-next-link a');
+var $search_prev = $('#islandora-solr-search-prev-link a');
+
+$search_return.addClass('green button');
+$search_next.addClass('button');
+$search_prev.addClass('button');
