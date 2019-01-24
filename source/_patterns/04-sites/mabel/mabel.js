a1ba3262c974cd745e37e2465b6bd77af0666393
masons7 <masons7@wwu.edu>
2019-01-23 16:00:38 -0800

    improvements to object page styles

--- source/_patterns/04-sites/mabel/mabel.js
+++ source/_patterns/04-sites/mabel/mabel.js
@@ -3,10 +3,15 @@ var $jail = $('#block-islandora-compound-object-compound-jail-display');
 var $prev = $('#islandora-compound-previous-link');
 var $seq = $('#islandora-compound-sequence-position');
 var $next = $('#islandora-compound-next-link');
+var $nav_container = $('<div class="compound-navigation"></div>');
 
-$prev.insertBefore($jail);
-$seq.insertBefore($jail);
-$next.insertBefore($jail);
+$nav_container.addClass('compound-navigation');
+
+$nav_container.insertBefore($jail);
+
+$nav_container.append($prev);
+$nav_container.append($seq);
+$nav_container.append($next);
 
 /* Remove clip icon and make it a descriptive link */
 var $clip = $('#clip');
