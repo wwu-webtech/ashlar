3fa28afcec6c7cd45811beee87881df9dd68b227
masons7 <masons7@wwu.edu>
2019-03-27 12:42:31 -0700

    link to advanced search being placed below search form

--- source/_patterns/01-molecules/search/mabel-search/mabel-search.js
+++ source/_patterns/01-molecules/search/mabel-search/mabel-search.js
@@ -1,6 +1,7 @@
 var $search_container = $('#islandora-search-container' , context);
 var $simple_search = $('#block-islandora-solr-simple', context);
+var $advanced_search_link =$('<a class="advanced-search-link" href="/advanced-search">Advanced Search</a>');
 
 /* Move search blocks into splash search container */
 $search_container.append($simple_search);
-$search_container.append($advanced_search);
+$search_container.append($advanced_search_link);
