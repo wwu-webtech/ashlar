ba50fbf85196eb4f64681bcb54768bbba46d592c
masons7 <masons7@wwu.edu>
2019-04-03 08:09:35 -0700

    combine pattern lab install scripts into one

--- patternlab-install.sh
+++ patternlab-install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+# install
 lando composer create-project -n pattern-lab/edition-twig-standard pattern-lab
 cd pattern-lab
 lando composer require aleksip/plugin-data-transform
+
+# provision
+lando php pattern-lab/core/console -c --set sourceDir=../source
+lando php pattern-lab/core/console -c --set twigAutoescape=0
