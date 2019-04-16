491a471e5c6dc58b0afb8d637b1ac96e51ea720d
Carly Gerard <carly.gerard@wwu.edu>
2019-04-16 12:01:42 -0700

    LSG-99 updated major content switcher with proper roles

--- source/_patterns/01-molecules/content/switcher/switcher.js
+++ source/_patterns/01-molecules/content/switcher/switcher.js
@@ -1,9 +1,26 @@
-$('.content-switcher button', context).click(function () {
-  $('.content-switcher button').removeClass('active');
-  $('.content-switcher button').attr('disabled', false);
+var $switchButton = $('.content-switcher button', context);
+var $thisPanel = $('.content-switcher-container .active', context);
+
+$('.content-switcher').attr('role', 'tablist');
+$('.content-switcher li').attr('role', 'presentation');
+$switchButton.attr('role', 'tab');
+$('.content-switcher button.active').attr('aria-selected', 'true');
+$('.content-switcher button:not(.active)').attr({'aria-selected' : 'false', 'tabindex' : '-1'});
+$thisPanel.attr('tabindex', '0');
+
+$switchButton.click(function () {
+  $switchButton.removeClass('active');
+  $switchButton.attr('aria-selected', 'false');
+  $switchButton.attr('tabindex', '-1');
+  // $switchButton.attr('disabled', false);
+
   var $thisSwitch = $(this).attr('class');
   $(this).addClass('active');
-  $(this).attr('disabled', true);
+  $(this).attr('aria-selected', 'true');
+  $(this).removeAttr('tabindex');
+  $thisPanel.attr('tabindex', '0');
+  // $(this).attr('disabled', true);
+
   $('.content-switcher-container .content:not(.' + $thisSwitch + ')').fadeOut();
   $('.content-switcher-container .content.' + $thisSwitch).fadeIn();
 });
