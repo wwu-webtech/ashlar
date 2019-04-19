340c2ec70b1d0b4ccded6cddb467a5e99a3bfe3f
Carly Gerard <carly.gerard@wwu.edu>
2019-04-19 10:53:18 -0700

    aria-controls/labelledby, vars declared

--- source/_patterns/01-molecules/content/switcher/switcher.js
+++ source/_patterns/01-molecules/content/switcher/switcher.js
@@ -1,6 +1,21 @@
-$('.content-switcher button', context).click(function () {
-  $('.content-switcher button').removeClass('active');
-  $('.content-switcher button').attr('disabled', false);
+var $tablist = $('.content-switcher', context);
+var $tabButton = $('.content-switcher button', context)
+var $panels = $('.content-switcher-container > .content', context);
+var $selectedPanel = $('.content-switcher-container > .content.active', context);
+
+$tablist.attr('role', 'tablist');
+$tablist.children('li').attr('role', 'presentation');
+$tabButton.attr('role', 'tab');
+
+$tabButton.hasClass('active').attr('aria-selected', 'true');
+$tabButton.not('active').attr({'aria-selected' : 'false', 'tabindex' : '-1'});
+$panels.attr({'tabindex' : '0', 'role' : 'tabpanel'});
+
+$tabButton.click(function () {
+  var $otherButtons = $('.content-switcher button');
+
+  $otherButtons.removeClass('active');
+  $otherButtons.attr('disabled', false);
   var $thisSwitch = $(this).attr('class');
   $(this).addClass('active');
   $(this).attr('disabled', true);
