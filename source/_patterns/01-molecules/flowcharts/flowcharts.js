9448af3416884f6be7a31c84d98ba1fd29215e48
Carly Gerard <carly.gerard@wwu.edu>
2019-05-10 16:59:32 -0700

    removing js, don't think it's necessary at this point

--- source/_patterns/01-molecules/flowcharts/flowcharts.js
+++
@@ -1,7 +0,0 @@
-// JS for flowchart management
-var $flowchartList = $('.flowchart', context);
-var $levelA = $flowchartList.find('.level-a');
-var $levelB = $flowchartList.find('.level-b');
-var $levelC = $flowchartList.find('.level-c');
-var $aLastLi = $levelA.find('li:last');
-var $count = 0;
