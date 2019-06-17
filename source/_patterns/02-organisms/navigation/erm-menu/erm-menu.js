1b53c06de533a1c666d5391eae98f4475db1a9be
masons7 <masons7@wwu.edu>
2019-06-17 10:33:32 -0700

    scaffold for erm menu pattern

---
+++ source/_patterns/02-organisms/navigation/erm-menu/erm-menu.js
@@ -0,0 +1,11 @@
+/* If the menu block is present */
+// Remove the nav .quick-links
+// Remove the .search
+// Remove the main navigation
+// Remove the splash region (first conten block gets moved into splash)
+
+// Show the open menu toggle on desktop
+// Change the menu toggle behavior (perhaps change the classes?) so that it hides/shows the #block-errmmenu instead
+
+
+// #block-ermmenu is styled to be a full page menu with water background
