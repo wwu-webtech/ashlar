b0eafdcd977aebd1e4c664c780e0edb748a9adcd
masons7 <masons7@wwu.edu>
2019-06-27 09:51:50 -0700

    add more structure to form

--- source/_patterns/02-organisms/email-signature/email-signature.js
+++ source/_patterns/02-organisms/email-signature/email-signature.js
@@ -1,5 +1,5 @@
-var generateButton = document.getElementById("generate_signature");
-var copyButton = document.getElementById("copy_button");
+var generateButton = document.getElementById('generate_signature');
+var copyButton = document.getElementById('copy_button');
 
 generateButton.onclick = function() {
   var name = document.getElementById('full_name');
@@ -10,14 +10,14 @@ generateButton.onclick = function() {
   var phone_one =  document.getElementById('phone_one');
   var phone_two =  document.getElementById('phone_two');
 
-  name = (name.value) ? name.value : "";
-  pronouns = (pronouns.value) ? " | " + pronouns.value : "";
-  job_title = (job_title.value) ? job_title.value : "";
-  department = (department.value) ? " | " + department.value : "";
-  email = (email.value) ? email.value : "";
-  phone_one = (phone_one.value) ? phone_one.value : "";
-  phone_two = (phone_two.value) ? phone_two.value : "";
-  
+  name = (name.value) ? name.value : '';
+  pronouns = (pronouns.value) ? ' | ' + pronouns.value : '';
+  job_title = (job_title.value) ? job_title.value : '';
+  department = (department.value) ? ' | ' + department.value : '';
+  email = (email.value) ? email.value : '';
+  phone_one = (phone_one.value) ? phone_one.value : '';
+  phone_two = (phone_two.value) ? phone_two.value : '';
+
   document.getElementById('field_name').innerText = name;
   document.getElementById('field_pronouns').innerText = pronouns;
   document.getElementById('field_job_title').innerText = job_title;
@@ -26,20 +26,23 @@ generateButton.onclick = function() {
   if(phone_one) { document.getElementById('field_phone_one').innerHTML = ' | <a href="tel:' + phone_one + '">' + phone_one + '</a>' } else { document.getElementById('field_phone_one').innerHTML= '' };
   if(phone_two) { document.getElementById('field_phone_two').innerHTML = ' | <a href="tel:' + phone_two + '">' + phone_two + '</a>' } else { document.getElementById('field_phone_two').innerHTML= '' };
 
+  copyButton.innerText ='Copy to Clipboard';
   copyButton.focus();
 }
 
 copyButton.onclick = function(){
   if (document.selection) {
     var range = document.body.createTextRange();
-    range.moveToElementText(document.getElementById("email_signature"));
+    range.moveToElementText(document.getElementById('email_signature'));
     range.select().createTextRange();
-    document.execCommand("copy");
+    document.execCommand('copy');
 
   } else if (window.getSelection) {
     var range = document.createRange();
-    range.selectNode(document.getElementById("email_signature"));
+    range.selectNode(document.getElementById('email_signature'));
     window.getSelection().addRange(range);
-    document.execCommand("copy");
+    document.execCommand('copy');
   }
+
+  copyButton.innerText ='Signature copied!';
 };
