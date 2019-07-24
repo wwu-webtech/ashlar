28b33864086eed43b7e29bc33a1a743a7e1b3ae1
Carly Gerard <carly.gerard@wwu.edu>
2019-07-24 11:25:25 -0700

    moving ARIA attributes from JS to static text

--- source/_patterns/02-organisms/quicksearch/quicksearch.js
+++ source/_patterns/02-organisms/quicksearch/quicksearch.js
@@ -31,10 +31,10 @@ var $letterMenu = $("#quick-search-list-nav");
 
 // ===Functions and attributes to enhance UX of filter===
 // Insert aria attr's to make div accessible:
-$results_text.attr('aria-live', 'polite');
-$results_text.attr('aria-atomic', 'true');
-$wrapper.attr('role', 'status');
-$results_text.attr('aria-labelledby', 'quick-search-input');
+// $results_text.attr('aria-live', 'polite');
+// $results_text.attr('aria-atomic', 'true');
+// $wrapper.attr('role', 'status');
+// $results_text.attr('aria-labelledby', 'quick-search-input');
 
 /* Prevent a disabled letter from being selected through tab-navigation or
  * scrolling to top of page on click.
