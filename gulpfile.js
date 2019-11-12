c20cfbb7f37db7d7f69f9f4e960731b48051a92d
masons7 <masons7@wwu.edu>
2019-11-12 09:05:52 -0800

    Revert "Merged release-november-12-2019 into master"
    This reverts commit 376c88aadf52b944aadbed872a436491382eceb9, reversing
    changes made to dd8bbf8068935a25d39760708fb1004ffebf510e.

--- gulpfile.js
+++ gulpfile.js
@@ -60,7 +60,7 @@
     jsFile: 'wwu-styleguide.js',
     jsSrc: [
       'source/_patterns/**/*.js',
-      '!source/_patterns/02-molecules/d7-accordion-alert/d7-accordion-alert.js',
+      '!source/_patterns/01-molecules/d7-accordion-alert/d7-accordion-alert.js',
       'dist/js/*.js',
       '!dist/js/acalog.js',
       'source/js/menu-extras.js'
