cb7678e2fdac845a61bcda2bef5eb76063ba17f7
Carly Gerard <carly.gerard@wwu.edu>
2020-02-26 13:39:13 -0800

    fixes switcher bug, moves prepend/append into two separate functions

--- source/_patterns/02-molecules/switcher/switcher.js
+++ source/_patterns/02-molecules/switcher/switcher.js
@@ -1,13 +1,15 @@
 var $tablist = $('.content-switcher', context);
 var $tabButton = $('.content-switcher button', context);
+var $activeButton = $('.content-switcher button.active', context);
 var $panels = $('.content-switcher-container .content', context);
 var $selectedPanel = $('.content-switcher-container .content.active', context);
 var $leftArrow = '<span class="material-icons show" aria-hidden="true">chevron_left</span>';
 var $rightArrow = '<span class="material-icons show" aria-hidden="true">chevron_right</span>';
 
 
-$('.content-switcher button.active').attr('aria-selected', 'true');
-$('.content-switcher button.active').prepend($leftArrow).append($rightArrow);
+$activeButton.attr('aria-selected', 'true');
+$activeButton.prepend($leftArrow);
+$activeButton.append($rightArrow);
 $('.content-switcher button:not(.active)').attr({'aria-selected': 'false', 'tabindex': '-1'});
 
 $tabButton.click(function () {
