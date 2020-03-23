d8ec3aca75bd869c86667d2896651a81bc93bca3
Carly Gerard <carly.gerard@wwu.edu>
2020-03-23 10:29:49 -0700

    including target="blank" variation, formatting

--- source/_patterns/01-atoms/text/links/external-links-a11y.js
+++ source/_patterns/01-atoms/text/links/external-links-a11y.js
@@ -1,7 +1,6 @@
 // add 'open in new tab' icon and text to external links. NOTE: This isn't a recommended practice for all links, but adds a11y info in case the link doesn't open in the same window
 
-var $linkExternal = $('a[target="_blank"]', context);
-
+var $linkExternal = $('a[target="_blank"], a[target="blank"]', context);
 var $openIcon = '<span class="material-icons" aria-hidden="true">open_in_new</span>';
 var $openText = '<span class="visually-hidden">(opens in new window)</span>';
 
