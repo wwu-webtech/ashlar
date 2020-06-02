509819715b9a6dde0ba71f08e481056a371cfafd
masons7 <masons7@wwu.edu>
2020-06-02 13:15:46 -0700

    update search

--- source/_patterns/02-molecules/search/search.js
+++ source/_patterns/02-molecules/search/search.js
@@ -1,9 +1,9 @@
 var $toggle_search = $('.toggle-search', context);
 var $search_icon = $toggle_search.children('.material-icons');
 var $search_box = $('#search-box');
-var $toggle_prefix = $toggle_search.children('.toggle-prefix');
-var $toggle_text = $toggle_search.children('.toggle-text');
-var $toggle_suffix = $toggle_search.children('.toggle-suffix');
+var $search_toggle_prefix = $toggle_search.children('.toggle-prefix');
+var $search_toggle_text = $toggle_search.children('.toggle-text');
+var $search_toggle_suffix = $toggle_search.children('.toggle-suffix');
 
 function toggle_search(event) {
   $toggle_search.off('click', toggle_search);
@@ -20,16 +20,16 @@ function toggle_search(event) {
 
         if ($toggle_search.hasClass('.is-expanded')) {
           $search_icon.text('close');
-          $toggle_prefix.html('');
-          $toggle_text.html('Close');
-          $toggle_suffix.html(' Search');
+          $search_toggle_prefix.html('');
+          $search_toggle_text.html('Close');
+          $search_toggle_suffix.html(' Search');
           $search_box.focus()
         }
         else {
           $search_icon.text('search');
-          $toggle_prefix.html('Open ');
-          $toggle_text.html('Search');
-          $toggle_suffix.html('');
+          $search_toggle_prefix.html('Open ');
+          $search_toggle_text.html('Search');
+          $search_toggle_suffix.html('');
         }
 
         $toggle_search.on('click', toggle_search);
