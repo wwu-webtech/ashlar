a5602900e11e6f967cdbd5f751e99dcd65271b80
Carly Gerard <carly.gerard@wwu.edu>
2020-07-24 14:36:25 -0700

    move from featherlight to new file

---
+++ source/js/featherlight-extras.js
@@ -0,0 +1,14 @@
+
+	// patch featherlight, semantically hide main page content while iframe modal is open
+	var $playLink = $('a[data-featherlight="iframe"]');
+	var $behindFeatherlight = $('.page, .skip-link, .page-content');
+
+	$playLink.featherlight({
+	  beforeOpen: function() {
+		$behindFeatherlight.attr('aria-hidden', 'true');
+	  },
+	  afterClose: function() {
+		$behindFeatherlight.removeAttr('aria-hidden');
+		$playLink.focus();
+	  }
+	});
