dee4338bfcbc43906758c42aab207ad39d134b46
Carly Gerard <carly.gerard@wwu.edu>
2020-07-24 14:42:42 -0700

    again formatting, additional comments

--- source/js/featherlight-extras.js
+++ source/js/featherlight-extras.js
@@ -1,7 +1,10 @@
 
 // patch featherlight, semantically hide main page content while iframe modal is open
+// stays in jQuery due to featherlight dependency
+
 var $playLink = $('a[data-featherlight="iframe"]');
 var $behindFeatherlight = $('.page, .skip-link, .page-content');
+
 $playLink.featherlight({
   beforeOpen: function() {
 	$behindFeatherlight.attr('aria-hidden', 'true');
