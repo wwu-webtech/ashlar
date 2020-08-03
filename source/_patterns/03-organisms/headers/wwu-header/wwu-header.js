bc23aa23fe2452e47d791ee0c2c1615b9c1fa8be
Carly Gerard <carly.gerard@wwu.edu>
2020-08-03 13:45:02 -0700

    DS-277 fix for backwards aria-expanded on mobile menu

--- source/_patterns/03-organisms/headers/wwu-header/wwu-header.js
+++ source/_patterns/03-organisms/headers/wwu-header/wwu-header.js
@@ -4,24 +4,49 @@ var $toggle_prefix = $menu_toggle.children('.toggle-prefix');
 var $toggle_text = $menu_toggle.children('.toggle-text');
 var $toggle_suffix = $menu_toggle.children('.toggle-suffix');
 var $last_focused;
-var $menu_link = $('.main-navigation .ultimenu .ultimenu__item .ultimenu__link');
 var $inputs = $(document).find('select, button, a, input, textarea', context);
 var $page = $('.skip-link, .splash, .make-waves, .header-navigation .site-name, .toggle-open-menu, .page-content, .region--footer', context);
+
+// aria-expanded vars
+var $menu_item = $('.ultimenu__item');
+var $menu_link = $menu_item.children('.ultimenu__link');
 
 function toggle_menu(event) {
   $menu_toggle.off('click', toggle_menu);
 
   if ($menu_toggle.hasClass('is-expanded')) {
     toggle_close_menu(event);
+    $menu_link.attr('aria-hidden', 'true');
+    $menu_link.attr('tabindex', '-1');
   }
   else {
     toggle_open_menu(event);
+    $menu_link.removeAttr('aria-hidden tabindex');
+    ariaFix();
   }
+
+  // aria-expanded fix
+  function ariaFix() {
+    $menu_link.removeAttr('aria-hidden aria-expanded');
+    $('.has-ultimenu').find($menu_link).attr({
+      'role': 'button',
+      'aria-expanded': false
+    });
+  
+    $menu_link.click(function(){
+      if ($(this).parent().hasClass('has-ultimenu')) {
+        $(this).attr('aria-expanded', 'true');
+      }
+      else {
+        $(this).attr('aria-expanded', 'true');
+      }  
+    });
+  }
+// end aria-expanded fix
 }
 
 function toggle_open_menu(event) {
   $last_focused = document.activeElement;
-
   $('.header-navigation').show();
   $menu_icon.text('close');
   $toggle_prefix.html('');
@@ -41,7 +66,6 @@ function toggle_open_menu(event) {
         $menu_toggle.on('click', toggle_menu);
         $menu_toggle.focus();
         $inputs.not('.toggle-menu, .header-navigation a, .header-navigation button, .ultimenu__flyout a').attr('tabindex', '-1');
-        $menu_link.removeAttr('aria-hidden');
         $page.not('.header-navigation').attr('aria-hidden', 'true');
       }
     }
