bacf4ee395d9a697848dd2cf38d8c83891b7afe0
Carly Gerard <carly.gerard@wwu.edu>
2020-08-06 16:03:13 -0700

    DS-276 converts new tab script from jQuery to plain JS

--- source/_patterns/01-atoms/text/links/external-links-a11y.js
+++ source/_patterns/01-atoms/text/links/external-links-a11y.js
@@ -1,8 +1,18 @@
 // add 'open in new tab' icon and text to external links. NOTE: This isn't a recommended practice for all links, but adds a11y info in case the link doesn't open in the same window
 
-var $linkExternal = $('a[target="_blank"], a[target="blank"]', context);
-var $openIcon = '<span class="material-icons" aria-hidden="true">open_in_new</span>';
-var $openText = '<span class="visually-hidden">(opens in new window)</span>';
+var newTab = document.querySelectorAll('a[target="_blank"], a[target="blank"]', context);
 
-$linkExternal.append($openIcon);
-$linkExternal.append($openText);
+for (var i = 0; i < newTab.length; i++) {
+  // icon
+  var icon = document.createElement('span');
+  icon.innerHTML = 'open_in_new';
+  icon.classList.add('material-icons');
+  icon.setAttribute('aria-hidden', 'true');
+
+  // visually-hidden text
+  var text = document.createElement('span');
+  text.innerHTML = ' (opens in new tab)';
+  text.classList.add('visually-hidden');
+
+  newTab[i].append(text, icon);
+}
