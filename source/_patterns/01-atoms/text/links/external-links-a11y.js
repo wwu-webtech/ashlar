dcf491a67eaea9838138f6d4e7a01fcce15b95b2
Carly Gerard <carly.gerard@wwu.edu>
2020-08-07 12:27:11 -0700

    using Array.from and forEach, only run on live page

--- source/_patterns/01-atoms/text/links/external-links-a11y.js
+++ source/_patterns/01-atoms/text/links/external-links-a11y.js
@@ -1,18 +1,25 @@
 // add 'open in new tab' icon and text to external links. NOTE: This isn't a recommended practice for all links, but adds a11y info in case the link doesn't open in the same window
 
+
 var newTab = document.querySelectorAll('a[target="_blank"], a[target="blank"]', context);
+var page = document.querySelector('body');
 
-for (var i = 0; i < newTab.length; i++) {
-  // icon
-  var icon = document.createElement('span');
-  icon.innerHTML = 'open_in_new';
-  icon.classList.add('material-icons');
-  icon.setAttribute('aria-hidden', 'true');
+// only run if on a live page
+if (!page.classList.contains('adminimal-admin-toolbar')) {
+  var newTabArr = Array.from(newTab);
 
-  // visually-hidden text
-  var text = document.createElement('span');
-  text.innerHTML = ' (opens in new tab)';
-  text.classList.add('visually-hidden');
-
-  newTab[i].append(text, icon);
+  newTabArr.forEach(function(link) {
+    var icon = document.createElement('span');
+    icon.innerHTML = 'open_in_new';
+    icon.classList.add('material-icons');
+    icon.setAttribute('aria-hidden', 'true');
+  
+    // visually-hidden text
+    var text = document.createElement('span');
+    text.innerHTML = ' (opens in new tab)';
+    text.classList.add('visually-hidden');
+  
+    link.append(text, icon);
+  });
 }
+
