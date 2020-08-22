ad3ec361e432fe3f8335b3ffd1ef3a3cdd2291a0
Carly Gerard <carly.gerard@wwu.edu>
2020-08-21 17:57:36 -0700

    changing lightbox heading name

--- source/_patterns/02-molecules/lightbox/lightbox.js
+++ source/_patterns/02-molecules/lightbox/lightbox.js
@@ -1,20 +1,5 @@
-// On initial play button click
-//
-// Show alert dialog
-// Focus on close button
-// Set background to aria-hidden
-// remove tabindex from non-dialog elements
-// 
-// On close button click
-// 
-// Set focus to play button
-// Hide the modal dialog
-// 
-// for each button
-// add click event
-// this button is event target
-// var lightbox is next dialog sibling
-
+// To do
+// style container
 
 var lbPlayButton = document.querySelectorAll('.play-link');
 var playButtonArr = Array.from(lbPlayButton);
@@ -37,13 +22,22 @@ closeButton.classList.add('close-dialog')
 closeButton.innerHTML = '<span class="material-icons" aria-hidden="true">clear</span> <span class="visually-hidden">Close dialog</span>'
 lbContainer.appendChild(closeButton);
 
+  // visually-hidden heading, needed to name dialog
+var lbHeading = document.createElement('h2');
+lbHeading.innerHTML = 'Lightbox';
+lbHeading.setAttribute('id', 'dialog-heading');
+lbHeading.classList.add('visually-hidden');
+lbContainer.appendChild(lbHeading);
+
   // video iframe
 var video = document.createElement('div');
-video.classList.add('embed-container');
+video.classList.add('embed-container', 'lightbox-content');
+video.setAttribute('id', 'lightbox-content')
 
 var iframe = document.createElement('iframe');
 iframe.setAttribute('title', '');
 iframe.setAttribute('src', '');
+
 video.appendChild(iframe);
 lbContainer.appendChild(video);
 
@@ -56,41 +50,36 @@ playButtonArr.forEach(function(button){
     button.addEventListener('click', lightboxModal);
 
     function lightboxModal() {
+        var pageBackground = document.querySelectorAll('div:not(.lightbox-dialog), .skip-link');
         playButtonFocused = document.activeElement;
-        lbContainer.classList.remove('hidden');
+
+        // set aria and tabindex attrs
+        for (var i = 0; i < pageBackground.length; i++) {
+            pageBackground[i].setAttribute('aria-hidden', 'true');
+          }
         for (var i = 0; i < bgFocusable.length; i++) {
           bgFocusable[i].setAttribute('tabindex', '-1');
         }
+        // add iframe src and titles
+        iframe.setAttribute('title', this.dataset.title);
+        iframe.setAttribute('src', this.dataset.url);
+        lbContainer.classList.replace('hidden', 'shown'); // reveal modal
 
         var closeDialog = lbContainer.querySelector('.close-dialog');
+        closeDialog.focus();
+        
+        // on close, return focus to button pressed
         closeDialog.addEventListener('click', function(){
-          lbContainer.classList.add('hidden');
+          lbContainer.classList.replace('shown', 'hidden');
+          iframe.setAttribute('title', '');
+          iframe.setAttribute('src', '');
+          for (var i = 0; i < pageBackground.length; i++) {
+            pageBackground[i].removeAttribute('aria-hidden');
+          }
           for (var i = 0; i < bgFocusable.length; i++) {
             bgFocusable[i].removeAttribute('tabindex');
           }  
           playButtonFocused.focus();
-    })
-  };
+        })
+    };
 });
-
-// var nonDialogContent = document.getElementsByTagName('div');
-// var bgFocusable = document.querySelectorAll('a, button:not(.lightbox-close-dialog), textarea, select, input');
-
-// var playButtonFocused;
-
-// lbPlayButton.addEventListener('click', function() {
-//     var nonDialogContentArr = Array.from(nonDialogContent);
-//     var bgFocusableArr = Array.from(bgFocusable);
-
-//     playButtonFocused = document.activeElement;
-//     closeButton.focus();
-//     lightbox.classList.remove('hidden');
-//     nonDialogContentArr.forEach(function(div){
-//       if (!div.hasChildNodes('.lightbox-dialog')) {
-//           div.setAttribute('aria-hidden', 'true');
-//       }
-//     })
-//     bgFocusableArr.forEach(function(ui){
-//         ui.setAttribute('tabindex', '-1');
-//     })
-//   });
