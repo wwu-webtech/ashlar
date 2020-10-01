d815ddc477689b035c693028b0713951a0f1f9c6
Stephanie Mason <masons7@wwu.edu>
2020-10-01 11:34:47 -0700

    refine open/close of menu

--- source/_patterns/02-molecules/display-preferences/display-preferences.js
+++ source/_patterns/02-molecules/display-preferences/display-preferences.js
@@ -2,14 +2,26 @@ var toggle_display_preferences = document.querySelector('.toggle-display-prefere
 var display_preferences_menu = document.querySelector('.display-preferences-menu');
 
 toggle_display_preferences.onclick = function() {
-  if (window.getComputedStyle(display_preferences_menu).display === 'flex') {
+  if (display_preferences_menu.classList.contains('open')) {
     toggle_display_preferences.setAttribute('aria-expanded', false);
-    display_preferences_menu.setAttribute('hidden', '');
-		display_preferences_menu.style.display='none';
-		return;
-	} else {
+    toggle_display_preferences.querySelector('.material-icons').innerText = 'settings';
+    toggle_display_preferences.querySelector('.toggle-prefix').innerText = 'Open ';
+    toggle_display_preferences.querySelector('.icon-text').innerText = 'Display Preferences';
+
+    display_preferences_menu.setAttribute('aria-hidden', true);
+    display_preferences_menu.classList.remove('open');
+    display_preferences_menu.classList.add('closed');
+    return;
+  } else {
     toggle_display_preferences.setAttribute('aria-expanded', true);
+    toggle_display_preferences.querySelector('.material-icons').innerText = 'close';
+    toggle_display_preferences.querySelector('.toggle-prefix').innerText = '';
+    toggle_display_preferences.querySelector('.icon-text').innerText = 'Close Preferences';
+
+    display_preferences_menu.setAttribute('aria-hidden', false);
     display_preferences_menu.removeAttribute('hidden');
-    display_preferences_menu.style.display='flex';
+    display_preferences_menu.classList.remove('closed');
+    display_preferences_menu.classList.add('open');
+    return;
   }
 }
