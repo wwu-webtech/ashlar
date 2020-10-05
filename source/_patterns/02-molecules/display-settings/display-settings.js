57d8d61f60a031805c9ce9c916e66fb0e777cd22
Stephanie Mason <masons7@wwu.edu>
2020-10-05 16:11:57 -0700

    new menu functions

--- source/_patterns/02-molecules/display-settings/display-settings.js
+++ source/_patterns/02-molecules/display-settings/display-settings.js
@@ -1,29 +1,31 @@
-var display_pref_toggle = document.querySelector('.toggle-display-settings');
-var display_preferences_menu = document.querySelector('.display-settings-menu');
+var display_toggle = document.querySelector('.toggle-display-settings');
+var display_menu = document.querySelector('.display-settings-menu');
 
-function toggle_preferences() {
-  if (display_preferences_menu.classList.contains('open')) {
-    display_pref_toggle.setAttribute('aria-expanded', false);
-    display_pref_toggle.querySelector('.material-icons').innerText = 'settings';
-    display_pref_toggle.querySelector('.toggle-prefix').innerText = 'Open ';
-    display_pref_toggle.querySelector('.icon-text').innerText = 'Display Preferences';
+function toggle_settings() {
+  if (display_menu.classList.contains('open')) {
+    display_toggle.setAttribute('aria-expanded', false);
+    display_toggle.querySelector('.material-icons').innerText = 'settings';
+    display_toggle.querySelector('.toggle-prefix').innerText = 'Open ';
+    display_toggle.querySelector('.toggle-text').innerText = 'Display';
+    display_toggle.querySelector('.toggle-suffix').innerText = ' Settings';
 
-    display_preferences_menu.setAttribute('aria-hidden', true);
-    display_preferences_menu.classList.remove('open');
-    display_preferences_menu.classList.add('closed');
+    display_menu.setAttribute('aria-hidden', true);
+    display_menu.classList.remove('open');
+    display_menu.classList.add('closed');
     return;
   } else {
-    display_pref_toggle.setAttribute('aria-expanded', true);
-    display_pref_toggle.querySelector('.material-icons').innerText = 'close';
-    display_pref_toggle.querySelector('.toggle-prefix').innerText = '';
-    display_pref_toggle.querySelector('.icon-text').innerText = 'Close Preferences';
+    display_toggle.setAttribute('aria-expanded', true);
+    display_toggle.querySelector('.material-icons').innerText = 'close';
+    display_toggle.querySelector('.toggle-prefix').innerText = '';
+    display_toggle.querySelector('.toggle-text').innerText = 'Close';
+    display_toggle.querySelector('.toggle-suffix').innerText = ' Display Settings';
 
-    display_preferences_menu.setAttribute('aria-hidden', false);
-    display_preferences_menu.removeAttribute('hidden');
-    display_preferences_menu.classList.remove('closed');
-    display_preferences_menu.classList.add('open');
+    display_menu.setAttribute('aria-hidden', false);
+    display_menu.removeAttribute('hidden');
+    display_menu.classList.remove('closed');
+    display_menu.classList.add('open');
     return;
   }
 }
 
-display_pref_toggle.addEventListener('click', toggle_preferences);
+display_toggle.addEventListener('click', toggle_settings);
--- source/_patterns/03-organisms/headers/wwu-header/wwu-header.js
+++ source/_patterns/03-organisms/headers/wwu-header/wwu-header.js
@@ -51,6 +51,11 @@ function open_mobile_menu() {
   mobile_menu_wrapper.classList.add('open');
   mobile_menu_wrapper.classList.remove('closed');
   mobile_menu_wrapper.insertBefore(mobile_menu_toggle, header_quick_links);
+
+  mobile_menu_toggle.querySelector('.material-icons').innerText = "close";
+  mobile_menu_toggle.querySelector('.toggle-prefix').innerText = "";
+  mobile_menu_toggle.querySelector('.toggle-text').innerText = "Close";
+  mobile_menu_toggle.querySelector('.toggle-suffix').innerText = " the menu";
 }
 
 function close_mobile_menu() {
@@ -59,6 +64,11 @@ function close_mobile_menu() {
   mobile_menu_wrapper.classList.remove('open');
   mobile_menu_wrapper.classList.add('closed');
   western_header.insertBefore(mobile_menu_toggle, header_site_name)
+
+  mobile_menu_toggle.querySelector('.material-icons').innerText = "menu";
+  mobile_menu_toggle.querySelector('.toggle-prefix').innerText = "Open the ";
+  mobile_menu_toggle.querySelector('.toggle-text').innerText = "Menu";
+  mobile_menu_toggle.querySelector('.toggle-suffix').innerText = "";
 }
 
 function toggle_mobile_menu() {
@@ -73,118 +83,3 @@ function toggle_mobile_menu() {
 position_elements();
 window.addEventListener('resize', position_elements);
 mobile_menu_toggle.addEventListener('click', toggle_mobile_menu);
-
-
-
-/*
-var $menu_toggle = $('.toggle-menu', context);
-var $menu_icon = $menu_toggle.children('.material-icons');
-var $toggle_prefix = $menu_toggle.children('.toggle-prefix');
-var $toggle_text = $menu_toggle.children('.toggle-text');
-var $toggle_suffix = $menu_toggle.children('.toggle-suffix');
-var $last_focused;
-var $inputs = $(document).find('select, button, a, input, textarea', context);
-var $page = $('.skip-link, .splash, .make-waves, .header-navigation .site-name, .toggle-open-menu, .page-content, .region--footer', context);
-
-// aria-expanded vars
-var $menu_item = $('.ultimenu__item', context);
-var $menu_link = $menu_item.children('.ultimenu__link');
-
-function toggle_menu(event) {
-$menu_toggle.off('click', toggle_menu);
-
-if ($menu_toggle.hasClass('is-expanded')) {
-toggle_close_menu(event);
-}
-else {
-toggle_open_menu(event);
-}
-}
-
-function toggle_open_menu(event) {
-$last_focused = document.activeElement;
-$('.header-navigation').show();
-$menu_icon.text('close');
-$toggle_prefix.html('');
-$toggle_text.html('Close');
-$toggle_suffix.html(' the Menu');
-
-$('.header-navigation').animate(
-{
-right: '0',
-opacity: '1'
-},
-{
-duration: 'normal',
-easing: 'swing',
-complete: function () {
-$menu_toggle.addClass('is-expanded');
-$menu_toggle.on('click', toggle_menu);
-$menu_toggle.focus();
-$inputs.not('.toggle-menu, .header-navigation a, .header-navigation button, .ultimenu__flyout a').attr('tabindex', '-1');
-$page.not('.header-navigation').attr('aria-hidden', 'true');
-$menu_link.removeAttr('aria-hidden aria-expanded tabindex');
-$('.has-ultimenu').find($menu_link).attr({
-'role': 'button',
-'aria-expanded': false
-});
-}
-}
-);
-}
-
-function toggle_close_menu(event) {
-$menu_icon.text('menu');
-$toggle_prefix.html('Open the ');
-$toggle_text.html('Menu');
-$toggle_suffix.html('');
-$('.header-navigation').animate(
-{
-right: '-100%',
-opacity: '0'
-},
-{
-duration: 'normal',
-easing: 'swing',
-complete: function () {
-$('.header-navigation').hide();
-$('#dropLangMenu').hide();
-$menu_toggle.removeClass('is-expanded');
-$page.not('.header-navigation').removeAttr('aria-hidden');
-$inputs.not('.header-navigation a, .header-navigation button').removeAttr('tabindex');
-$menu_link.attr({'aria-hidden': 'true', 'tabindex': '-1'});
-$menu_toggle.on('click', toggle_menu);
-$last_focused.focus();
-}
-}
-);
-}
-
-// START desktop/mobile resize toggle
-var $window = $(window, context);
-
-function ariaMobile() {
-var $tablet_breakpoint = 840;
-
-if ($window.width() <= $tablet_breakpoint) {
-$menu_link.attr({'aria-hidden': 'true', 'tabindex': '-1'});
-}
-else {
-$menu_link.removeAttr('tabindex aria-hidden');
-}
-}
-
-function bindHandlers() {
-$window.unbind('resize', ariaMobile);
-
-if ($window.width() <= 841) {
-$window.resize(ariaMobile);
-}
-}
-
-$window.resize(bindHandlers);
-bindHandlers();
-// END desktop/mobile resize toggle
-
-$menu_toggle.on('click', toggle_menu);
-*/
