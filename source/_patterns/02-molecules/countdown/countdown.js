41312ba5516d6e19d7cc0f289c741ef1bb992ae0
Stephanie Mason <masons7@wwu.edu>
2020-11-18 13:35:16 -0800

    basic scaffold

---
+++ source/_patterns/02-molecules/countdown/countdown.js
@@ -0,0 +1,15 @@
+var second = 1000,
+  minute = second * 60,
+  hour = minute * 60,
+  day = hour * 24;
+
+var countDown = new Date('Sep 22, 2020 15:30:00').getTime(),
+x = setInterval(function() {
+  var now = new Date().getTime(),
+  distance = countDown - now;
+
+  document.getElementById('days').innerText = Math.floor(distance / (day)),
+  document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+  document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+  document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+}, second)
