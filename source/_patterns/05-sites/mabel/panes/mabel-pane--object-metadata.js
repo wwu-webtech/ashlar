87efa059ab8265f10f82870c4ca80acdf0bee34f
Stephanie Mason <masons7@wwu.edu>
2021-02-10 15:38:06 -0800

    declare variables for anon function

--- source/_patterns/05-sites/mabel/panes/mabel-pane--object-metadata.js
+++ source/_patterns/05-sites/mabel/panes/mabel-pane--object-metadata.js
@@ -4,17 +4,17 @@ var how_many_extras = extra_metadata.length; /* count now to prevent infinite lo
 var metadata_containers = document.getElementsByClassName("more-metadata");
 
 /* Move extra meta data into one container */
-for (i = 0; i < how_many_extras; i++) {
+for (var i = 0; i < how_many_extras; i++) {
   metadata_containers[0].appendChild(extra_metadata[i]);
 }
 
 /* For for other containers, create a copy and add it */
-for (i = 1; i < metadata_containers.length; i++) {
+for (var i = 1; i < metadata_containers.length; i++) {
   /* Make a copy of any extra metadata */
   var copied_metadata = new Array();
 
   /* Move copy into the container */
-  for (j = 0; j < how_many_extras; j++) {
+  for (var j = 0; j < how_many_extras; j++) {
     copied_metadata = extra_metadata[j].cloneNode(true);
     metadata_containers[i].appendChild(copied_metadata);
   }
