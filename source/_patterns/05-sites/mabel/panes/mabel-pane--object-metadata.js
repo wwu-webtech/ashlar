0b7cf412bdf85996e97dc33a9fe74aa0a5efe4d6
Stephanie Mason <masons7@wwu.edu>
2021-02-11 13:36:43 -0800

    use tabs to move content to prevent viewer breaking

--- source/_patterns/05-sites/mabel/panes/mabel-pane--object-metadata.js
+++ source/_patterns/05-sites/mabel/panes/mabel-pane--object-metadata.js
@@ -1,21 +1,38 @@
-/* Move object metadata into switcher tabs */
-var extra_metadata = document.getElementsByClassName("extra-metadata");
-var how_many_extras = extra_metadata.length; /* count now to prevent infinite loop */
-var metadata_containers = document.getElementsByClassName("more-metadata");
+/* For compound object pages */
 
-/* Move extra meta data into one container */
-for (var i = 0; i < how_many_extras; i++) {
-  metadata_containers[0].appendChild(extra_metadata[i]);
-}
+/* When the third tab is clicked, move things from the first two into it */
+var firstTab = document.getElementById('mabel-object-switch-button-1');
+var secondTab = document.getElementById('mabel-object-switch-button-2');
+var thirdTab = document.getElementById('mabel-object-switch-button-3');
 
-/* For for other containers, create a copy and add it */
-for (var i = 1; i < metadata_containers.length; i++) {
-  /* Make a copy of any extra metadata */
-  var copied_metadata = new Array();
+var firstContainer = document.getElementById('mabel-object-switch-content-1');
+var secondContainer = document.getElementById('mabel-object-switch-content-2');
+var thirdContainerLeft = document.getElementById('mabel-viewer-container');
+var thirdContainerRight = document.getElementById('mabel-metadata-container');
 
-  /* Move copy into the container */
-  for (var j = 0; j < how_many_extras; j++) {
-    copied_metadata = extra_metadata[j].cloneNode(true);
-    metadata_containers[i].appendChild(copied_metadata);
+var imageViewer = document.getElementsByClassName('islandora-large-image-content-wrapper');
+var metadata = document.getElementsByClassName('islandora-metadata');
+
+function splitContent() {
+  for(var i = 0; i < imageViewer.length; i++) {
+    firstContainer.appendChild(imageViewer[i]);
+  }
+
+  for(var i = 0; i < metadata.length; i++) {
+    secondContainer.appendChild(metadata[i]);
   }
 }
+
+function mergeContent() {
+  for(var i = 0; i < imageViewer.length; i++) {
+    thirdContainerLeft.appendChild(imageViewer[i]);
+  }
+
+  for(var i = 0; i < metadata.length; i++) {
+    thirdContainerRight.appendChild(metadata[i]);
+  }
+}
+
+firstTab.addEventListener("click", splitContent);
+secondTab.addEventListener("click", splitContent);
+thirdTab.addEventListener("click", mergeContent);
