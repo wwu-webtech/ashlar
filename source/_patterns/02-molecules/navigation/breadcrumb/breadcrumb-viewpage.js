7eb5b34ac17059f2f0d8c41b7022decc2f38338d
Carly Gerard <carly.gerard@wwu.edu>
2021-03-08 13:01:44 -0800

    fix: adjust breadcrumb structure on view page displays
    The breadcrumb preprocess recently added only works on nodes, which
    doesn't account for page view displays like profile pages, taxonomy
    term pages, etc. This checks if it's that view, and attaches this script if true.
    Makes the aria-current more accurate
    
    Resolves CDS-1638, see DS-338

---
+++ source/_patterns/02-molecules/navigation/breadcrumb/breadcrumb-viewpage.js
@@ -0,0 +1,33 @@
+/* Prevent multiple calls in Drupal */
+if (context !== document) {
+  return;
+}
+
+var breadcrumb_list = document.querySelector(
+  ".block--system-breadcrumb-block .breadcrumb ol"
+);
+
+if (breadcrumb_list) {
+  var previous_items = document.querySelectorAll(".breadcrumb ol li a");
+
+  for (var i = 0; i < previous_items.length; i++) {
+    previous_items[i].removeAttribute("aria-current");
+    previous_items[i].removeAttribute("class");
+  }
+
+  var page_title = document.querySelector(".page-title");
+  var profile_page_title = document.querySelector(".view--profile-page h1");
+  var current_listitem = document.createElement("li");
+  var current_link = document.createElement("a");
+
+  current_link.setAttribute("href", window.location.pathname);
+  current_link.setAttribute("aria-current", "page");
+  current_link.classList.add("bold");
+  if (profile_page_title) current_link.innerText = profile_page_title.innerText;
+  else current_link.innerText = page_title.innerText;
+
+  current_listitem.appendChild(current_link);
+  breadcrumb_list.appendChild(current_listitem);
+} else {
+  return;
+}
