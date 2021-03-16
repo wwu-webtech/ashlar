9d97be2ad39d9d36b0765b2818aa2e31707e3be5
Carly Gerard <carly.gerard@wwu.edu>
2021-03-16 15:03:27 -0700

    fix: switch to keyup listeners rather than keydown where needed
    One of the techniques for satisfying the newer pointer cancellation requirement is to
    use keyup rather than keydown for key-specific functions (on Esc press, on arrow press, etc.).
    Some code was still using keydown, so this adjusts to use keyup.
    
    The exception is the lightbox focus trap, where keydown is needed to simulate tabbing to the close button/CTA link either way.

--- source/_patterns/02-molecules/switcher/switcher.js
+++ source/_patterns/02-molecules/switcher/switcher.js
@@ -1,67 +1,69 @@
-var $tablist = $('.content-switcher', context);
-$('.content-switcher-container button').appendTo('.content-switcher');
-var $tabButton = $('.content-switcher button', context);
-var $panels = $('.content-switcher-container .content', context);
-var $selectedPanel = $('.content-switcher-container .content.active', context);
+var $tablist = $(".content-switcher", context);
+$(".content-switcher-container button").appendTo(".content-switcher");
+var $tabButton = $(".content-switcher button", context);
+var $panels = $(".content-switcher-container .content", context);
+var $selectedPanel = $(".content-switcher-container .content.active", context);
 
-$tabButton.first().addClass('active');
-$panels.first().addClass('active');
+$tabButton.first().addClass("active");
+$panels.first().addClass("active");
 
-var $activeButton = $('.content-switcher button.active', context);
-var $leftArrow = '<span class="material-icons show" aria-hidden="true">chevron_left</span>';
-var $rightArrow = '<span class="material-icons show" aria-hidden="true">chevron_right</span>';
+var $activeButton = $(".content-switcher button.active", context);
+var $leftArrow =
+  '<span class="material-icons show" aria-hidden="true">chevron_left</span>';
+var $rightArrow =
+  '<span class="material-icons show" aria-hidden="true">chevron_right</span>';
 
-$activeButton.attr('aria-selected', 'true');
+$activeButton.attr("aria-selected", "true");
 $activeButton.prepend($leftArrow);
 $activeButton.append($rightArrow);
-$('.content-switcher button:not(.active)').attr({
-  'aria-selected': 'false', 
-  'tabindex': '-1'
+$(".content-switcher button:not(.active)").attr({
+  "aria-selected": "false",
+  tabindex: "-1",
 });
 
 $tabButton.click(function () {
-  var $otherButtons = $('.content-switcher button');
-  $otherButtons.removeClass('active');
-  $otherButtons.attr('aria-selected', 'false');
-  $otherButtons.attr('tabindex', '-1');
-  $otherButtons.children('.material-icons').remove();
-  var $thisSwitch = $(this).attr('class');
-  $(this).addClass('active');
+  var $otherButtons = $(".content-switcher button");
+  $otherButtons.removeClass("active");
+  $otherButtons.attr("aria-selected", "false");
+  $otherButtons.attr("tabindex", "-1");
+  $otherButtons.children(".material-icons").remove();
+  var $thisSwitch = $(this).attr("class");
+  $(this).addClass("active");
   $(this).prepend($leftArrow);
   $(this).append($rightArrow);
-  $(this).attr('aria-selected', 'true');
-  $(this).removeAttr('tabindex');
+  $(this).attr("aria-selected", "true");
+  $(this).removeAttr("tabindex");
 
-  $('.content-switcher-container .content:not(.' + $thisSwitch + ')').fadeOut(200);
-  $('.content-switcher-container .content.' + $thisSwitch).fadeIn(200);
+  $(".content-switcher-container .content:not(." + $thisSwitch + ")").fadeOut(
+    200
+  );
+  $(".content-switcher-container .content." + $thisSwitch).fadeIn(200);
 });
 
-$tabButton.keydown(function (event) {
+$tabButton.on("keyup", function (event) {
   var $key = event.keyCode;
-  var $selected = $('.content-switcher button.active', context);
-  var $selectedPanel = $('#' + $selected.attr('aria-controls'), context);
+  var $selected = $(".content-switcher button.active", context);
+  var $selectedPanel = $("#" + $selected.attr("aria-controls"), context);
 
   if ($key === 37) {
     // find previous tab, if we are on first => activate last
-    $selectedPanel.removeClass('active');
-    if ($selected.is('.content-switcher button:first-child')) {
-      $('.content-switcher button:last-child').click().focus();
-      $('.content-switcher-container:last-child').addClass('active');
-    }
-    else {
+    $selectedPanel.removeClass("active");
+    if ($selected.is(".content-switcher button:first-child")) {
+      $(".content-switcher button:last-child").click().focus();
+      $(".content-switcher-container:last-child").addClass("active");
+    } else {
       $selected.prev($tabButton).click().focus();
     }
     event.preventDefault();
   }
 
   if ($key === 39) {
-  // find next tab, if we are on last => activate first
-    $selectedPanel.removeClass('active');
-    if ($selected.is('.content-switcher button:last-child')) {
-      $('.content-switcher button:first-child').click().focus();
-      $selectedPanel.addClass('active');
-    }
-    else {
+    // find next tab, if we are on last => activate first
+    $selectedPanel.removeClass("active");
+    if ($selected.is(".content-switcher button:last-child")) {
+      $(".content-switcher button:first-child").click().focus();
+      $selectedPanel.addClass("active");
+    } else {
       $selected.next($tabButton).click().focus();
     }
     event.preventDefault();
