825e0b033b058ccd16d9ee3b4969f33da90544fd
Carly Gerard <carly.gerard@wwu.edu>
2021-10-20 16:46:16 -0700

    DS-398 adds keyboard/a11y support for accordions in views

--- source/_patterns/02-molecules/accordion/accordion.js
+++ source/_patterns/02-molecules/accordion/accordion.js
@@ -1,23 +1,39 @@
-/* Add aria labels to all buttons */
-var $accordionButton = $(".accordion-set .expand", context);
+// Set vars and aria attributes
+var $accordionToggles = $(".accordion-set .expand", context);
+var $accordionButton = $(".accordion-set button.expand", context);
+var $accordionMockButton = $(".accordion-set a.expand", context);
 var $accordionContent = $(".accordion-set .content");
-$accordionButton.filter(".is-expanded").attr("aria-expanded", "true");
-$accordionButton.not(".is-expanded").attr("aria-expanded", "false");
 
-/* Set unique ids and aria-controls */
+$accordionToggles.filter(".is-expanded").attr("aria-expanded", "true");
+$accordionToggles.not(".is-expanded").attr("aria-expanded", "false");
+
 for (var i = 0; i < $accordionContent.length; i++) {
   $accordionContent[i].setAttribute("id", "accordion-content-" + i);
 }
 
-$accordionButton.each(function () {
+$accordionToggles.each(function () {
   var $contentID = $(this).parent().siblings($accordionContent).attr("id");
   $(this).attr("aria-controls", $contentID);
 });
 
-/* Toggle expansion */
-$accordionButton.click(function () {
-  var accordionButtonClicked = $(this);
-  var accordionContent = $(this).parent().siblings(".content");
+// Kbd support function for mock button toggles
+function keyPressed(event) {
+  if (event.type === "click") {
+    return true;
+  } else if (event.type === "keypress") {
+    var code = event.charCode || event.keyCode;
+    if (code === 32 || code === 13) {
+      return true;
+    }
+  } else {
+    return false;
+  }
+}
+
+// Toggle expansion/collapse
+function toggleExpansion(button) {
+  var accordionButtonClicked = button;
+  var accordionContent = button.parent().siblings(".content");
 
   accordionButtonClicked.toggleClass("is-expanded");
   accordionContent.slideToggle();
@@ -30,17 +46,32 @@ $accordionButton.click(function () {
 
   accordionButtonClicked.filter(".is-expanded").attr("aria-expanded", "true");
   accordionButtonClicked.not(".is-expanded").attr("aria-expanded", "false");
-});
+}
 
-// Close with Esc key
-$accordionButton.on("keyup", function (e) {
+// On Esc press, close accordion content
+function closeAccordion(event) {
   var accordionButtonClicked = $(this);
   var accordionContent = accordionButtonClicked.parent().siblings(".content");
+  var $code = event.charCode || event.keyCode;
 
-  if (e.keyCode === 27) {
+  if ($code === 27) {
     accordionContent.slideUp();
     accordionButtonClicked.removeClass("is-expanded");
     accordionButtonClicked.children(".material-icons").text("add");
     accordionButtonClicked.attr("aria-expanded", "false");
   }
+}
+
+// Run accordion events
+$accordionButton.on("click", function () {
+  toggleExpansion($(this));
 });
+
+$accordionMockButton.on("click keypress", function (event) {
+  event.preventDefault();
+  if (keyPressed(event) === true) {
+    toggleExpansion($(this));
+  }
+});
+
+$accordionToggles.on("keyup", closeAccordion);
