c1f3b7d22c1018a484b3a1091d800f1077473598
Carly Gerard <carly.gerard@wwu.edu>
2021-11-16 15:29:33 -0800

    initial working config for skipto

---
+++ dist/js/skipto-extras.js
@@ -0,0 +1,10 @@
+var SkipToConfig = {
+  settings: {
+    skipTo: {
+      landmarks:
+        'main, [role="main"], [role="search"], nav,[role="navigation"], footer, [role="contentinfo"]',
+      headings: "main h1, main h2, main h3",
+      customClass: "skipto--custom",
+    },
+  },
+};
