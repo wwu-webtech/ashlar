51defd969ef3936992a5342831da59d05b0c3558
Carly Gerard <carly.gerard@wwu.edu>
2021-11-16 15:59:02 -0800

    class tweaks, hide skip link if skip button exists

--- dist/js/skipto-extras.js
+++ dist/js/skipto-extras.js
@@ -4,7 +4,12 @@ var SkipToConfig = {
       landmarks:
         'main, [role="main"], [role="search"], nav,[role="navigation"], footer, [role="contentinfo"]',
       headings: "main h1, main h2, main h3",
-      customClass: "skipto--custom",
+      customClass: "skip-to--custom",
     },
   },
 };
+
+const skipto_button = document.getElementsByClassName("skipto--custom");
+const skip_link = document.querySelector(".skip-link");
+
+if (skipto_button) skip_link.classList.add("hidden");
