c5d13554b03540cb7f7acb4a86fbadd80b4bd6b9
Carly Gerard <carly.gerard@wwu.edu>
2021-11-17 14:19:48 -0800

    cleans up external link JS file, now works in all contexts

--- source/_patterns/01-atoms/links/external-links-a11y.js
+++ source/_patterns/01-atoms/links/external-links-a11y.js
@@ -1,25 +1,26 @@
-// add 'open in new tab' icon and text to external links. NOTE: This isn't a recommended practice for all links, but adds a11y info in case the link doesn't open in the same window
+// Prevent multiple calls in Drupal
 
-
-var newTab = document.querySelectorAll('a[target="_blank"], a[target="blank"]', context);
-var $signedIn = document.querySelector('body').classList.contains('adminimal-admin-toolbar');
-
-// only run if on a live page
-if (!$signedIn) {
-  var newTabArr = Array.from(newTab);
-
-  newTabArr.forEach(function(link) {
-    var icon = document.createElement('span');
-    icon.innerHTML = 'open_in_new';
-    icon.classList.add('material-icons');
-    icon.setAttribute('aria-hidden', 'true');
-  
-    // visually-hidden text
-    var text = document.createElement('span');
-    text.innerHTML = ' (opens in new tab)';
-    text.classList.add('visually-hidden');
-  
-    link.append(text, icon);
-  });
+if (context !== document) {
+  return;
 }
 
+// add 'open in new tab' icon and text to external links.
+// NOTE: This isn't a recommended practice for all links, but adds a11y info in case the link doesn't open in the same window
+
+const targetBlank = document.querySelectorAll(
+  '.page a[target="_blank"], .page a[target="blank"]',
+  context
+);
+
+for (let link of targetBlank) {
+  let text = document.createElement("span");
+  text.innerHTML = " (opens in new tab)";
+  text.classList.add("visually-hidden");
+
+  let icon = document.createElement("span");
+  icon.innerHTML = "open_in_new";
+  icon.classList.add("material-icons");
+  icon.setAttribute("aria-hidden", "true");
+
+  link.append(text, icon);
+}
