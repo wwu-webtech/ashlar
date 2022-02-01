145f8b463ca465a7d51eb96346377deca4dfa9f8
Stephanie Mason <masons7@wwu.edu>
2022-02-01 13:26:45 -0800

    try different initialization

--- dist/js/skipto-config.js
+++ dist/js/skipto-config.js
@@ -3,12 +3,31 @@ var SkipToConfig = {
     skipTo: {
       landmarks:
         'main, [role="main"], header, [role="banner"], [role="search"], nav,[role="navigation"], footer, [role="contentinfo"]',
-      displayOption: "popup",
+      displayOption: "static",
+      attachElement: "wwu-pre-header",
       headerLabel: "Header",
       footerLabel: "Footer",
       asideLabel: "Aside",
       positionLeft: "0",
       buttonBackgroundColor: "transparent",
+      buttonLabel: "Skip to Content",
     },
   },
 };
+
+/*------------------------------------------------------------------------------
+Remove extra skip link
+---------------------------------------------------------------------------*/
+const observer = new MutationObserver((mutations, obs) => {
+  const wwu_skip_link = document.querySelector(".skip-link");
+  if (wwu_skip_link) {
+    wwu_skip_link.remove();
+    obs.disconnect();
+    return;
+  }
+});
+
+observer.observe(document, {
+  childList: true,
+  subtree: true,
+});
