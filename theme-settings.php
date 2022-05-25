ae4b14856f7cd1c833efeec040b19209dab8751d
Carly Gerard <gerardc@wwu.edu>
2022-05-24 17:24:14 -0700

    initial pass at theme setting for manual login

--- theme-settings.php
+++ theme-settings.php
@@ -29,4 +29,10 @@ function ashlar_form_system_theme_settings_alter(&$form, FormStateInterface &$fo
     '#title' => t('Display Western Alerts'),
     '#default_value' => theme_get_setting('alert_display'),
   );
+
+  $form['theme_settings']['manual_user_login'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Show Manual Login Form'),
+    '#default_value' => theme_get_setting('manual_user_login'),
+  );
 }
