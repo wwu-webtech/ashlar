dfa46bfb523220b9a1717666f6ede245c9428cff
masons7 <masons7@wwu.edu>
2022-09-21 15:22:10 -0700

    fix config comma

--- source/_docs/docusaurus.config.js
+++ source/_docs/docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'wwu-stephanie', // Usually your GitHub org/user name.
   projectName: 'ashlar', // Usually your repo name.
-  deploymentBranch: 'masons7/docusaurus'
+  deploymentBranch: 'masons7/docusaurus',
   trailingSlash: false,
 
   // Even if you don't use internalization, you can use this field to set useful
