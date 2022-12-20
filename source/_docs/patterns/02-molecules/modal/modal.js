187cc9ae48454d1b0947b8b6bb9668e458cf4e06
masons7 <masons7@wwu.edu>
2022-12-20 15:11:51 -0800

    modal molecule

--- source/_docs/patterns/02-molecules/modal/modal.js
+++ source/_docs/patterns/02-molecules/modal/modal.js
@@ -5,11 +5,11 @@ if (
         const modal_template = document.createElement("template");
         modal_template.innerHTML = `
         <div class="wrapper closed">
-        <div class="modal">
+        <div class="modal">            
         </div>
         </div>
         
-        <button aria-expanded="false"></button>
+        <button class="open-modal" aria-expanded="false"></button>
         `;
         
         class WWUModal extends HTMLElement {
@@ -21,43 +21,57 @@ if (
                 const content = this.innerHTML;
                 this.innerHTML = ``;
                 this.appendChild(modal_template.content.cloneNode(true));     
-                this.querySelector(".modal").innerHTML = content; 
+                this.querySelector(".modal").innerHTML = `<button class="close-modal"><span class="material-icons" aria-hidden="true">close</span>Close</button>` + content; 
                 
                 
-                const button = this.querySelector("button");
+                const open_button = this.querySelector(".open-modal");
                 if (this.getAttribute("icon")) {
-                    button.innerHTML = `<span class="material-icons">${this.getAttribute("icon")}</span>${this.getAttribute("label")}`
+                    open_button.innerHTML = `<span class="material-icons" aria-hidden="true">${this.getAttribute("icon")}</span>${this.getAttribute("label")}`
                 }
                 else {
-                    button.innerText = this.getAttribute("label");
+                    open_button.innerText = this.getAttribute("label");
                 }
                 
-                button.addEventListener("click", open_modal);
-                button.addEventListener("keyup", esc_close);
+                open_button.addEventListener("click", open_modal);
+                this.addEventListener("keyup", esc_close);
+                this.querySelector(".wrapper").addEventListener("click", click_close);                
             }
         }
 
         window.customElements.define("wwu-modal", WWUModal);      
         
         function open_modal(){
+            const close_button = this.parentNode.querySelector(".close-modal");
             this.parentNode.querySelector(".wrapper").classList.remove("closed");
-            this.setAttribute("aria-expanded", true);
+            this.setAttribute("aria-expanded", true);            
+            close_button.focus();
         }
 
         function close_modal() {
             this.parentNode.querySelector(".wrapper").classList.add("closed");
             this.setAttribute("aria-expanded", false);
+
+                        
+            /* Remove and re-add iframes to stop video playback without needing an api */
+            const iframe = this.parentNode.querySelector("iframe");
+            const iframe_container = iframe.parentNode;
+            iframe.remove();
+            iframe_container.appendChild(iframe);
         }
         
         function esc_close(event){
             const key_pressed = event.code;
             
             if(key_pressed == "Escape") {
-                close_modal(this);
+                close_modal.call(this);
             }
         }
         
-        
+        function click_close(event) {
+            if (!event.target.classList.contains("modal")) {
+                close_modal.call(this);
+            }
+        }        
     }  
     
     
--- source/_docs/patterns/02-molecules/modal/modal.md
+++ source/_docs/patterns/02-molecules/modal/modal.md
@@ -2,6 +2,8 @@ import WWUModal from "./modal.js"
 
 # Modal
 
-<wwu-modal label="Play a Video" icon="play_arrow">
-Stuff
+<wwu-modal label="Play a Vimeo Video" icon="play_arrow">
+<div class="embed-container">
+    <iframe src="https://player.vimeo.com/video/668337172?h=15cc98305e&badge=0&autopause=0&player_id=0&app_id=58479/embed" allow="autoplay; fullscreen; picture-in-picture"></iframe>
+</div>
 </wwu-modal>
