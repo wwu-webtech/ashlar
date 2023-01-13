7e587c8ac4b356abea8c7ba17dc5f72c19c8044d
masons7 <masons7@wwu.edu>
2023-01-13 11:41:23 -0800

    switchers

--- source/_docs/docusaurus.config.js
+++ source/_docs/docusaurus.config.js
@@ -61,6 +61,7 @@ const config = {
             require.resolve('../../build/css/components/testimonial.css'),
             require.resolve('../../build/css/components/schedule.css'),
             require.resolve('../../build/css/components/statistic.css'),
+            require.resolve('../../build/css/components/switcher.css'),
           ],
         },
       }),
