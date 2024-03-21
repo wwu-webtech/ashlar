d714e290f34a0041fd15382f03d9bce4394a591b
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-03-21 11:32:49 -0700

    make changes to acalog.js based on experiments

--- dist/js/acalog.js
+++ dist/js/acalog.js
@@ -1,14 +1,15 @@
 (function($) {
   Drupal.behaviors.acalog = {
     attach: function(context, settings) {
+      /* Current Catalog ID. Note this may be a few numbers behind the coid that shows up in the URL */
+      const current_catoid = "18";
+      
+      /* Setting these attribnutes in acalogWidgetize just doesn't work, so we are setting them here instead */
+      $('.acalog').attr("data-acalog-data", "programs");
+      $('.acalog').attr("data-acalog-catalog-id", current_catoid);
       $('.acalog').acalogWidgetize({
         // gateway to western catalog
         gateway: 'https://catalog.wwu.edu',
-        // request for catalogs
-        data: 'catalogs',
-        // request for a specific catalog
-        // catalogName: "2019-2020 University Catalog"
-        catalogId: 13
       });
     }
   };
