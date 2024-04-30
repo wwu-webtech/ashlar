c860d7417138e8b461ae921d244e64fe7b589542
Carly Gerard <gerardc@wwu.edu>
2024-04-30 15:07:33 -0700

    dark mode contrast checks for molecules

--- source/_docs/patterns/02-molecules/schedule/schedule.cy.js
+++ source/_docs/patterns/02-molecules/schedule/schedule.cy.js
@@ -9,5 +9,19 @@ describe("molecule: schedule", () => {
       cy.injectAxe()
       cy.checkA11y(".theme-doc-markdown")
     })
+
+    it("passes color contrast in dark mode", () => {
+      cy.get('html').invoke('attr', 'data-theme', 'dark')
+      cy.injectAxe()
+      cy.checkA11y('.theme-doc-markdown', {
+        runOnly: {
+          type: 'tag',
+          values: ['wcag2a', 'wcag2aa']
+        },
+        rules: {
+          'color-contrast': {enabled: true}
+        }
+      })
+    })
   })
 })
