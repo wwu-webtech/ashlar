07af98d06c0277b0129a216f64e10cc2860fce1d
Carly Gerard <gerardc@wwu.edu>
2024-05-03 14:54:28 -0700

    Refactor molecule cypress tests into import format

--- source/_docs/patterns/02-molecules/accordion/accordion.cy.js
+++ source/_docs/patterns/02-molecules/accordion/accordion.cy.js
@@ -1,29 +1,12 @@
+import { runAutoA11yTests } from "../../../cypress/support/global"
+
 describe("molecule: accordion", () => {
   beforeEach(() => {
     cy.visit("/patterns/molecules/accordion")
     cy.get(".theme-doc-markdown").should("be.visible")
   })
 
-  context("automated accessibility tests", () => {
-    it("passes axe core accessibility tests", () => {
-      cy.injectAxe()
-      cy.checkA11y(".theme-doc-markdown")
-    })
-
-    it("passes color contrast in dark mode", () => {
-      cy.get("html").invoke("attr", "data-theme", "dark")
-      cy.injectAxe()
-      cy.checkA11y(".theme-doc-markdown", {
-        runOnly: {
-          type: "tag",
-          values: ["wcag2a", "wcag2aa"]
-        },
-        rules: {
-          "color-contrast": {enabled: true}
-        }
-      })
-    })
-  })
+  runAutoA11yTests();
 
   context("manual accessibility tests", () => {
     it("accordion items are focusable", () => {
