72d61a887a59fe3247c2e6edaece83d84051702c
Carly Gerard <gerardc@wwu.edu>
2024-05-03 14:54:28 -0700

    Refactor molecule cypress tests into import format

--- source/_docs/patterns/02-molecules/switcher/switcher.cy.js
+++ source/_docs/patterns/02-molecules/switcher/switcher.cy.js
@@ -1,27 +1,10 @@
+import { runAutoA11yTests } from "../../../cypress/support/global"
+
 describe("molecule: switcher", () => {
   beforeEach(() => {
     cy.visit("/patterns/molecules/switcher")
     cy.get(".theme-doc-markdown").should("be.visible")
   })
 
-  context("automated accessibility tests", () => {
-    it("passes axe core accessibility tests", () => {
-      cy.injectAxe()
-      cy.checkA11y(".theme-doc-markdown")
-    })
-
-    it("passes color contrast in dark mode", () => {
-      cy.get('html').invoke('attr', 'data-theme', 'dark')
-      cy.injectAxe()
-      cy.checkA11y('.theme-doc-markdown', {
-        runOnly: {
-          type: 'tag',
-          values: ['wcag2a', 'wcag2aa']
-        },
-        rules: {
-          'color-contrast': {enabled: true}
-        }
-      })
-    })
-  })
+  runAutoA11yTests();
 })
