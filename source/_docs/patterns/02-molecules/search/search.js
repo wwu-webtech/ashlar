9ce06018ec57a11a6c1743289803cba719fcae81
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-05-29 13:00:57 -0700

    add check for homepage

--- source/_docs/patterns/02-molecules/search/search.js
+++ source/_docs/patterns/02-molecules/search/search.js
@@ -5,32 +5,32 @@ if (
   const search_template = document.createElement("template");
   search_template.innerHTML = `
   <button class="toggle-settings" aria-expanded="false" aria-controls="search-menu">
-  <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#e8eaed"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><path d="M7,9H2V7h5V9z M7,12H2v2h5V12z M20.59,19l-3.83-3.83C15.96,15.69,15.02,16,14,16c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5 c0,1.02-0.31,1.96-0.83,2.75L22,17.59L20.59,19z M17,11c0-1.65-1.35-3-3-3s-3,1.35-3,3s1.35,3,3,3S17,12.65,17,11z M2,19h10v-2H2 V19z" /></g></svg></span>
-  <span class="toggle-text">Search Settings</span>
+      <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#e8eaed"><g><path d="M0,0h24v24H0V0z" fill="none" /></g><g><path d="M7,9H2V7h5V9z M7,12H2v2h5V12z M20.59,19l-3.83-3.83C15.96,15.69,15.02,16,14,16c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5 c0,1.02-0.31,1.96-0.83,2.75L22,17.59L20.59,19z M17,11c0-1.65-1.35-3-3-3s-3,1.35-3,3s1.35,3,3,3S17,12.65,17,11z M2,19h10v-2H2 V19z" /></g></svg></span>
+      <span class="toggle-text">Search Settings</span>
   </button>
-  
+
   <div id="search-menu" class="search-menu black-bg closed">
-  <fieldset class="search-selection">
-  <legend>Search Scope</legend>
-  <div class="radio">
-  <input id="site-search" name="search-select" value="site" type="radio" checked>
-  <label for="site-search">This department or website only</label>
+      <fieldset class="search-selection">
+          <legend>Search Scope</legend>
+          <div class="radio">
+              <input id="site-search" name="search-select" value="site" type="radio" checked>
+              <label for="site-search">This department or website only</label>
+          </div>
+          
+          <div class="radio">
+              <input id="full-search" name="search-select" value="full" type="radio">
+              <label for="full-search">All Western Washington University websites</label>
+          </div>
+      </fieldset>
   </div>
-  
-  <div class="radio">
-  <input id="full-search" name="search-select" value="full" type="radio">
-  <label for="full-search">All Western Washington University websites</label>
-  </div>
-  </fieldset>
-  </div>
-  
+
   <form class="search-area">
-  <label for="search-box" class="search-label">Search:</label>
-  <input id="search-box" name="q" type="search">
-  <button class="submit-search">
-  <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none" /><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /></svg></span>
-  <span class="toggle-text">Go</span>
-  </button>
+      <label for="search-box" class="search-label">Search:</label>
+      <input id="search-box" name="q" type="search">
+      <button class="submit-search">
+          <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none" /><path d="M15.5 14h-.79l-.28-.27C15.41 12.59 16 11.11 16 9.5 16 5.91 13.09 3 9.5 3S3 5.91 3 9.5 5.91 16 9.5 16c1.61 0 3.09-.59 4.23-1.57l.27.28v.79l5 4.99L20.49 19l-4.99-5zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5 14 7.01 14 9.5 11.99 14 9.5 14z" /></svg></span>
+          <span class="toggle-text">Go</span>
+      </button>
   </form>
   `;
   
@@ -46,11 +46,17 @@ if (
         this.appendChild(search_template.content.cloneNode(true));
         this.classList.add("element-created");
         this.querySelector(".search-area").addEventListener("submit", this.submitSearch.bind(this));
+
+        var subdomain = window.location.hostname.split('.')[0];
         
         const close_icon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>`;
         const settings_icon = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#e8eaed"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M7,9H2V7h5V9z M7,12H2v2h5V12z M20.59,19l-3.83-3.83C15.96,15.69,15.02,16,14,16c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5 c0,1.02-0.31,1.96-0.83,2.75L22,17.59L20.59,19z M17,11c0-1.65-1.35-3-3-3s-3,1.35-3,3s1.35,3,3,3S17,12.65,17,11z M2,19h10v-2H2 V19z"/></g></svg>`;        
         
-        
+        if (subdomain == "www") {
+          this.querySelector("#site-search").checked = false;
+          this.querySelector("#full-search").checked = true;
+        }
+
         /*------------------------------------------------------------------------------
         Menu Toggling
         ------------------------------------------------------------------------------*/
