5e197d4456599c08671cd8655e41cab2c566cf19
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-06-03 10:51:36 -0700

    set default to all sites on search site

--- source/_docs/patterns/02-molecules/search/search.js
+++ source/_docs/patterns/02-molecules/search/search.js
@@ -52,7 +52,7 @@ if (
         const close_icon = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg>`;
         const settings_icon = `<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#e8eaed"><g><path d="M0,0h24v24H0V0z" fill="none"/></g><g><path d="M7,9H2V7h5V9z M7,12H2v2h5V12z M20.59,19l-3.83-3.83C15.96,15.69,15.02,16,14,16c-2.76,0-5-2.24-5-5s2.24-5,5-5s5,2.24,5,5 c0,1.02-0.31,1.96-0.83,2.75L22,17.59L20.59,19z M17,11c0-1.65-1.35-3-3-3s-3,1.35-3,3s1.35,3,3,3S17,12.65,17,11z M2,19h10v-2H2 V19z"/></g></svg>`;        
         
-        if (subdomain == "www") {
+        if (subdomain == "www" || subdomain == "search") {
           this.querySelector("#site-search").checked = false;
           this.querySelector("#full-search").checked = true;
         }
