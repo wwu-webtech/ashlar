c6e7ab2780000f8da6e63c899350b596855a2d1e
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-06-17 12:51:13 -0700

    aria label & role for non-captioned tables

--- source/_docs/patterns/01-atoms/tables/table-a11y.js
+++ source/_docs/patterns/01-atoms/tables/table-a11y.js
@@ -7,19 +7,23 @@ if (
         const table = tables[i];
         
         if (!table.parentElement.classList.contains("responsive-table")){                        
-            var wrapper = document.createElement("div");
-            wrapper.classList.add("responsive-table");
-            wrapper.setAttribute("tabIndex", "0");
-            wrapper.setAttribute("role", "region");            
+            var new_wrapper = document.createElement("div");
             
-            if (table.querySelector("caption")) {
-                const id = Math.random().toString(16).slice(2);
-                table.querySelector("caption").setAttribute("id", id);
-                wrapper.setAttribute("aria-labelledby", id);                
-            }
-            
-            table.parentNode.insertBefore(wrapper, table);
-            wrapper.appendChild(table);
+            table.parentNode.insertBefore(new_wrapper, table);
+            new_wrapper.appendChild(table);
+        } 
+        
+        var wrapper = table.parentNode;
+        wrapper.classList.add("responsive-table");
+        wrapper.setAttribute("tabIndex", "0");
+        wrapper.setAttribute("role", "region");            
+        
+        if (table.querySelector("caption")) {
+            const id = Math.random().toString(16).slice(2);
+            table.querySelector("caption").setAttribute("id", id);
+            wrapper.setAttribute("aria-labelledby", id);                
+        } else {
+            wrapper.setAttribute("aria-label", `Table ${i + 1}`);
         }
     }
 }
