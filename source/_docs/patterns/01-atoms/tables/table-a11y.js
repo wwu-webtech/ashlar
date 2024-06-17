5f1baf2d3fb235f687674b915dc7a3ba463d96a0
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-06-17 08:55:39 -0700

    check for caption

--- source/_docs/patterns/01-atoms/tables/table-a11y.js
+++ source/_docs/patterns/01-atoms/tables/table-a11y.js
@@ -6,15 +6,17 @@ if (
     for (let i = 0; i < tables.length; i++) {
         const table = tables[i];
         
-        if (!table.parentElement.classList.contains("responsive-table")){            
-            const id = Math.random().toString(16).slice(2);
+        if (!table.parentElement.classList.contains("responsive-table")){                        
             var wrapper = document.createElement("div");
             wrapper.classList.add("responsive-table");
             wrapper.setAttribute("tabIndex", "0");
-            wrapper.setAttribute("role", "region");
-            wrapper.setAttribute("aria-labelledby", id);
+            wrapper.setAttribute("role", "region");            
             
-            table.querySelector("caption").setAttribute("id", id);
+            if (table.querySelector("caption")) {
+                const id = Math.random().toString(16).slice(2);
+                table.querySelector("caption").setAttribute("id", id);
+                wrapper.setAttribute("aria-labelledby", id);                
+            }
             
             table.parentNode.insertBefore(wrapper, table);
             wrapper.appendChild(table);
