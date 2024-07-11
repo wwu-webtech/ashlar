f9e21b72cd8528cfdbbbe7c1b58cf28cc79d878f
Carly Gerard <gerardc@wwu.edu>
2024-07-11 09:28:29 -0700

    disable collapse button by default if all items are collapsed

--- source/_docs/patterns/02-molecules/accordion/accordion.js
+++ source/_docs/patterns/02-molecules/accordion/accordion.js
@@ -154,6 +154,9 @@ if (
     // Expand/collapse all accordion items
     const button_collapse_all = document.createElement("button")
     button_collapse_all.classList.add('collapse-all');
+    if (!document.querySelector('wwu-accordion-item[expand]')) {
+      button_collapse_all.setAttribute('disabled', 'true');
+    }
     button_collapse_all.innerHTML = `
       <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/></svg></span>
       Collapse all sections
