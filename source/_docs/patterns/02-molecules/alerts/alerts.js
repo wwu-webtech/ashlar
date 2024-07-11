ec583dd82b3637fea0ff716dd4c68977f960be01
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-07-11 10:08:41 -0700

    reduce jquery to initialization files only

--- source/_docs/patterns/02-molecules/alerts/alerts.js
+++ source/_docs/patterns/02-molecules/alerts/alerts.js
@@ -1,64 +1,53 @@
-var emergency = $.getJSON('https://emergency.wwu.edu/api/alert/3');
-var weather = $.getJSON('https://emergency.wwu.edu/api/alert/4');
+function getJSON(url) {
+  return fetch(url)
+    .then(function(response) {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    });
+}
+
+var emergencyPromise = getJSON('https://emergency.wwu.edu/api/alert/3');
+var weatherPromise = getJSON('https://emergency.wwu.edu/api/alert/4');
 
 var handler = {
-
   has: function (obj, key) {
     var skey = String(key);
     var lskey = skey.toLowerCase();
     var uskey = lskey.charAt(0).toUpperCase() + skey.substr(1); // In case of camel case, using original string.
-
     return skey in obj || lskey in obj || uskey in obj;
   },
-
   get: function (obj, key) {
     var skey = String(key);
     var lskey = skey.toLowerCase();
     var uskey = lskey.charAt(0).toUpperCase() + skey.substr(1); // In case of camel case, using original string.
-
     return obj[skey] || obj[lskey] || obj[uskey] || undefined;
   }
-
 };
 
-function log_error(jqXHR, textStatus, errorThrown) {
-  console.log(errorThrown);
+function log_error(error) {
+  console.log(error.message);
 }
 
 function valid_response(data) {
-  if (!('title' in data)) {
-    return false;
-  }
-
-  if (!('content' in data)) {
-    return false;
-  }
-
-  if (!data.title) {
-    return false;
-  }
-
-  if (!data.content) {
-    return false;
-  }
-
-  return true;
+  return ('title' in data && 'content' in data && data.title && data.content);
 }
 
 function all_clear(data) {
   return data.title.trim().toUpperCase().includes('ALL CLEAR');
 }
 
-function display_alert($alert, data) {
-  var $title = $alert.children('.alert-header');
-  var $content = $alert.children('.alert-body');
+function display_alert(alertElement, data) {
+  var titleElement = alertElement.querySelector('.alert-header');
+  var contentElement = alertElement.querySelector('.alert-body');
 
-  $title.text(data.title);
-  $content.text(data.content);
-  $alert.show();
+  titleElement.textContent = data.title;
+  contentElement.textContent = data.content;
+  alertElement.style.display = 'block';
 }
 
-emergency.done(function (data) {
+emergencyPromise.then(function (data) {
   var proxy = new Proxy(data, handler);
 
   if (valid_response(proxy)) {
@@ -66,17 +55,16 @@ emergency.done(function (data) {
       console.log("Emergency alert JSON returned ALL CLEAR.");
     }
     else {
-      display_alert($('#alert-emergency', context), proxy);
+      var emergencyAlert = document.getElementById('alert-emergency');
+      display_alert(emergencyAlert, proxy);
     }
   }
   else {
     console.log("Emergency alert JSON was not in a valid format.");
   }
-});
+}).catch(log_error);
 
-emergency.fail(log_error);
-
-weather.done(function (data) {
+weatherPromise.then(function (data) {
   var proxy = new Proxy(data, handler);
 
   if (valid_response(proxy)) {
@@ -84,12 +72,11 @@ weather.done(function (data) {
       console.log("Weather alert JSON returned ALL CLEAR.");
     }
     else {
-      display_alert($('#alert-weather', context), proxy);
+      var weatherAlert = document.getElementById('alert-weather');
+      display_alert(weatherAlert, proxy);
     }
   }
   else {
     console.log("Weather alert JSON was not in a valid format.");
   }
-});
-
-weather.fail(log_error);
+}).catch(log_error);
