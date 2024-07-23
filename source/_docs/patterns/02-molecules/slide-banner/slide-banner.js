67aed8f3d34c63fc01379eaaee5fed5f46b568cf
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2024-07-23 15:40:58 -0700

    play/pause capability

--- source/_docs/patterns/02-molecules/slide-banner/slide-banner.js
+++ source/_docs/patterns/02-molecules/slide-banner/slide-banner.js
@@ -4,6 +4,10 @@ if (
   ) {
     const slide_banner_template = document.createElement("template");
     slide_banner_template.innerHTML = `    
+    <button class="playback play">
+      <svg xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 24 24" aria-hidden="true"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>
+      <span>pause</span>
+    </button>
     `;
     
     class WWUSlideBanner extends HTMLElement {
@@ -20,8 +24,10 @@ if (
           const slides = this.querySelectorAll("wwu-slide");
 
           for (let i = 0; i < slides.length; i++) { 
-            slides[i].innerHTML = `<img src="${slides[i].getAttribute('src')}">`;
+            slides[i].innerHTML = `<img src="${slides[i].getAttribute('src')}" alt="${slides[i].getAttribute('alt')}">`;
           }
+
+          this.querySelector("button").addEventListener("click", toggle_playback);       
         }
       }
     }
@@ -30,45 +36,23 @@ if (
       window.customElements.define("wwu-slide-banner", WWUSlideBanner);  
     }
     
-    function toggle_playback() {
-      const slideshow = this.parentNode.querySelector("slideshow");             
-      
+    function toggle_playback() {            
       if (this.classList.contains("play")) {
-        pause_slideshow(slideshow, this);
+        pause_slideshow(this);
       } else {
-        play_slideshow(slideshow, this);
+        play_slideshow(this);
       }
     }
     
-    function play_slideshow(vid, btn) {
-      vid.play().catch(function (error) {
-        console.log(error);
-      });
+    function play_slideshow(btn) {
       btn.classList.add("play");
-      btn.innerHTML = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>Pause</button>`;
+      btn.innerHTML = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M6 19h4V5H6v14zm8-14v14h4V5h-4z"/></svg>pause</button>`;
     }
     
-    function pause_slideshow(vid, btn) {
-      vid.pause();
+    function pause_slideshow(btn) {
       btn.classList.remove("play");
-      btn.innerHTML = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#fff"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>Play</button>`;
+      btn.innerHTML = `<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="18px" viewBox="0 0 24 24" width="18px"><path d="M0 0h24v24H0z" fill="none"/><path d="M8 5v14l11-7z"/></svg>play</button>`;
     }    
-    
-    function reduced_motion_check(pref, vid, btn) {
-      if (pref.matches) {
-        pause_slideshow(vid, btn);
-      } else {
-        play_slideshow(vid, btn);
-      }
-      
-      pref.onchange = (e) => {
-        if (e.matches) {
-          pause_slideshow(vid, btn)
-        } else {
-          play_slideshow(vid, btn)
-        }
-      }
-    }  
   } 
     
   
--- source/_docs/patterns/02-molecules/slide-banner/slide-banner.md
+++ source/_docs/patterns/02-molecules/slide-banner/slide-banner.md
@@ -5,8 +5,8 @@ import Placeholder2 from '@site/static/img/sampleheader.jpg';
 # Slide Banner
 
 <wwu-slide-banner class="black-overlay min-height--50vh">
-  <wwu-slide src={Placeholder}></wwu-slide>
-  <wwu-slide src={Placeholder2}></wwu-slide>
+  <wwu-slide src={Placeholder} alt="Autumn leaves"></wwu-slide>
+  <wwu-slide src={Placeholder2} alt="Western drone shot"></wwu-slide>
 
   <h2>Some Content</h2>
   <p>That sits on top of the slideshow</p>
