f46a4732c603e49732704e085e4694f3cd08b007
Carly Gerard <gerardc@wwu.edu>
2024-08-22 15:23:04 -0700

    add aria-describedby attr to expand all button

--- source/_docs/patterns/02-molecules/accordion/accordion.js
+++ source/_docs/patterns/02-molecules/accordion/accordion.js
@@ -170,6 +170,7 @@ if (
     
     const button_expand_all = document.createElement("button");
     button_expand_all.classList.add('expand-all');
+    button_expand_all.setAttribute("aria-describedby", "expand_all_hint_text")
     button_expand_all.innerHTML = `
       <span class="component-icon" aria-hidden="true"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px"><path d="M0 0h24v24H0z" fill="none"/><path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z"/></svg></span>
       Expand all sections
