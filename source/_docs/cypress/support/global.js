df71600290c9c4ea9c76981d397d9e149c31e317
Carly Gerard <gerardc@wwu.edu>
2024-10-31 10:44:10 -0700

    change code block class

--- source/_docs/cypress/support/global.js
+++ source/_docs/cypress/support/global.js
@@ -10,7 +10,7 @@ export function runAutoA11yTests() {
       cy.get("html").invoke("attr", "data-theme", "dark")
       cy.injectAxe()
       cy.checkA11y({
-        exclude: '.navbar--fixed-top, .theme-doc-sidebar-container, .theme-doc-breadcrumbs, .prism-code, .table-of-contents',
+        exclude: '.navbar--fixed-top, .theme-doc-sidebar-container, .theme-doc-breadcrumbs, .theme-code-block, .table-of-contents',
         runOnly: {
           type: "tag",
           values: ["wcag2a", "wcag2aa"]
