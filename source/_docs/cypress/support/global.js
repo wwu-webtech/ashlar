ce8168c4d70ec3638360cc35aacaf11708dbc2a1
Carly Gerard <gerardc@wwu.edu>
2024-10-31 10:43:20 -0700

    Exclude docusaurus and code blocks in dark mode contrast check

--- source/_docs/cypress/support/global.js
+++ source/_docs/cypress/support/global.js
@@ -9,7 +9,8 @@ export function runAutoA11yTests() {
     it("passes color contrast in dark mode", () => {
       cy.get("html").invoke("attr", "data-theme", "dark")
       cy.injectAxe()
-      cy.checkA11y(".theme-doc-markdown", {
+      cy.checkA11y({
+        exclude: '.navbar--fixed-top, .theme-doc-sidebar-container, .theme-doc-breadcrumbs, .prism-code, .table-of-contents',
         runOnly: {
           type: "tag",
           values: ["wcag2a", "wcag2aa"]
