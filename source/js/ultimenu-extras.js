83f42682ce12e455f4d9eb2b73aac80b57ee4052
Carly Gerard <gerardc@wwu.edu>
2025-07-28 15:55:08 -0700

    add ultimenu override so aria-expanded works on click

---
+++ source/js/ultimenu-extras.js
@@ -0,0 +1,29 @@
+(function ($, Drupal, once) {
+  Drupal.behaviors.ultimenuExtras = {
+    attach: function (context, settings) {
+      if (Drupal.ultimenu && typeof Drupal.ultimenu.onClickCaret === 'function') {
+        Drupal.ultimenu.onClickCaret = function (e) {
+          const $caret = e.target;
+          e.preventDefault();
+          e.stopPropagation();
+
+          const $link = $caret.closest('.ultimenu__link');
+          const $li = $link.closest('li');
+          const $flyout = $link.nextElementSibling;
+
+          if (!$flyout) return false;
+
+          const isExpanded = $li.classList.contains('is-uitem-expanded');
+
+          // Toggle aria-expanded
+          $link.setAttribute('aria-expanded', !isExpanded);
+
+          // Toggle Ultimenu's visual classes
+          $li.classList.toggle('is-uitem-expanded', !isExpanded);
+          $link.classList.toggle('is-ulink-active', !isExpanded);
+          $flyout.classList.toggle('is-flyout-expanded', !isExpanded);
+        };
+      }
+    }
+  };
+})(jQuery, Drupal, once);
