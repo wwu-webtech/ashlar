d34cff3d1dabef7d131bfcbed3680e08811d2232
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2026-01-22 10:56:40 -0800

    add theme option for loading autopilot

--- theme-settings.php
+++ theme-settings.php
@@ -32,6 +32,11 @@ function ashlar_form_system_theme_settings_alter(&$form, FormStateInterface &$fo
     '#title' => t('Load Javascript for dynamic catalog links'),
     '#default_value' => theme_get_setting('enable_acalog_widget'),
   );
+  $form['load_scripts']['enable_brightedge'] = array(
+    '#type' => 'checkbox',
+    '#title' => t('Load BrightEdge Autopilot'),
+    '#default_value' => theme_get_setting('enable_brightedge'),
+  );
 
   // Ashlar Custom Options
   $form['ashlar_options']['navigation_option'] = array(
