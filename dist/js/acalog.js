78148cc296dd36afa5d1e47881493459964ce826
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2026-08-28 12:14:17 -0700

    update widgetize code to fix links

--- dist/js/acalog.js
+++ dist/js/acalog.js
@@ -1,20 +1,17 @@
 (function($) {
   Drupal.behaviors.acalog = {
-    attach: function(context, settings) {
-      /* Current Catalog ID. Note this may be a few numbers behind the coid that shows up in the URL */
-      const current_catoid = "21";
-      
+    attach: function(context, settings) {      
       /* Setting these attribnutes in acalogWidgetize just doesn't work, so we are setting them here instead */
       $('.acalog').attr("data-acalog-data", "programs");
-      $('.acalog').attr("data-acalog-catalog-id", current_catoid);
       $('.acalog').acalogWidgetize({
         // gateway to western catalog
         gateway: 'https://catalog.wwu.edu',
+        data: 'catalogs',
+        catalogId: 1,
       });
 
       /* Slightly different class for making course links */
       $('.acalog-course').attr("data-acalog-data", "courses");
-      $('.acalog-course').attr("data-acalog-catalog-id", current_catoid);
       $('.acalog-course').acalogWidgetize({
         gateway: 'https://catalog.wwu.edu',
       });
