587e17c3c974fc2f485d00b660d965ac9935d4b5
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2026-08-28 12:31:05 -0700

    add feature to maintain original link text

--- dist/js/acalog.js
+++ dist/js/acalog.js
@@ -3,6 +3,9 @@
     attach: function(context, settings) {      
       /* Setting these attribnutes in acalogWidgetize just doesn't work, so we are setting them here instead */
       $('.acalog').attr("data-acalog-data", "programs");
+      $('.acalog').each(function () {
+        $(this).attr('data-acalog-link-text', $(this).text());
+      });
       $('.acalog').acalogWidgetize({
         // gateway to western catalog
         gateway: 'https://catalog.wwu.edu',
@@ -12,8 +15,13 @@
 
       /* Slightly different class for making course links */
       $('.acalog-course').attr("data-acalog-data", "courses");
+      $('.acalog-courses').each(function () {
+        $(this).attr('data-acalog-link-text', $(this).text());
+      });
       $('.acalog-course').acalogWidgetize({
         gateway: 'https://catalog.wwu.edu',
+        data: 'catalogs',
+        catalogId: 1,
       });
     }
   };
