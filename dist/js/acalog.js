06937e2c521bc05109e140dce1c9494cc5725cf8
Stephanie Lynn Mason Paulantis <masons7@wwu.edu>
2026-09-23 08:08:26 -0700

    fix catalog links

--- dist/js/acalog.js
+++ dist/js/acalog.js
@@ -3,25 +3,23 @@
     attach: function(context, settings) {      
       /* Setting these attribnutes in acalogWidgetize just doesn't work, so we are setting them here instead */
       $('.acalog').attr("data-acalog-data", "programs");
+      $('.acalog').attr("data-acalog-catalog-type", "default");
       $('.acalog').each(function () {
         $(this).attr('data-acalog-link-text', $(this).text());
       });
       $('.acalog').acalogWidgetize({
         // gateway to western catalog
         gateway: 'https://catalog.wwu.edu',
-        data: 'catalogs',
-        catalogId: 1,
       });
 
       /* Slightly different class for making course links */
       $('.acalog-course').attr("data-acalog-data", "courses");
+      $('.acalog-course').attr("data-acalog-catalog-type", "default");
       $('.acalog-courses').each(function () {
         $(this).attr('data-acalog-link-text', $(this).text());
       });
       $('.acalog-course').acalogWidgetize({
         gateway: 'https://catalog.wwu.edu',
-        data: 'catalogs',
-        catalogId: 1,
       });
     }
   };
